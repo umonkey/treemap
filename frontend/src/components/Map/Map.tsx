ac2248c27217af1e9bb2dfd8820334c83473aaec
Justin Forest <hex@umonkey.net>
2024-03-19 01:00:33 +0400

    Request markers for current viewport only

--- frontend/src/components/Map/Map.tsx
+++ frontend/src/components/Map/Map.tsx
@@ -1,27 +1,66 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 
 import { useMarkers } from "./hooks";
+import { ITreeInfo } from "../../services/api/types";
 
 import "./styles.css";
 
 export const Map = () => {
-  const { center, markers } = useMarkers();
+  const { center, markers, reload } = useMarkers();
+
+  const MapEventHandler = () => {
+    const map = useMapEvents({
+      click: () => {
+        console.debug("MAP CLICK");
+      },
+
+      load: () => {
+        reload(map.getBounds());
+      },
+
+      zoomend: () => {
+        reload(map.getBounds());
+      },
+
+      moveend: () => {
+        reload(map.getBounds());
+      },
+    });
+
+    /**
+     * This causes recursion, use a timer for now.
+     */
+    map.whenReady(() => {
+      setTimeout(() => {
+        reload(map.getBounds());
+      }, 100);
+    });
+
+    return null;
+  };
+
+  const Markers = () => {
+    const items = markers.map((marker: ITreeInfo, index: number) => (
+      <Marker key={index} position={[marker.lat, marker.lon]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    ));
+
+    return items;
+  };
 
   return (
     <MapContainer center={center} zoom={13} maxZoom={18} scrollWheelZoom={true} className="map">
+      <MapEventHandler />
+
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      {markers.map((marker, index) => (
-        <Marker key={index} position={[marker.lat, marker.lon]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      ))}
-
+      <Markers />
     </MapContainer>
   );
 };
--- frontend/src/services/api/TreeMapService.ts
+++ frontend/src/services/api/TreeMapService.ts
@@ -1,6 +1,10 @@
-import axios, { AxiosResponse, AxiosInstance, AxiosRequestConfig } from "axios";
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
 import { ITreeInfo } from "./types";
+
+interface ITreesResponse {
+  trees: ITreeInfo[];
+}
 
 export class TreeMapService {
   private readonly client: AxiosInstance;
@@ -16,12 +20,26 @@ export class TreeMapService {
     });
   }
 
-  public async getMarkers(): Promise<ITreeInfo[]> {
-    const res = await this.get("/v1/trees");
-    return res.data.trees;
+  public async getMarkers(params: {
+    north: number;
+    east: number;
+    south: number;
+    west: number;
+  }): Promise<ITreeInfo[]> {
+    const res = await this.get<ITreesResponse>("/v1/trees", {
+      params: {
+        n: params.north,
+        e: params.east,
+        s: params.south,
+        w: params.west,
+      }
+    });
+
+    return res.trees;
   }
 
-  private async get<T>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
-    return this.client.get<T>(url, config);
+  private async get<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
+    const res = await this.client.get<T>(url, config);
+    return res.data;
   }
 }
