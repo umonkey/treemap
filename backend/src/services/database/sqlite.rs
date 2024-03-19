5cdf1439fbce24b7721f07a1584020c46b2acd9d
Justin Forest <hex@umonkey.net>
2024-03-20 02:10:51 +0400

    Add unit tests for the trees service (#12)

--- backend/src/services/database/sqlite.rs
+++ backend/src/services/database/sqlite.rs
@@ -8,7 +8,7 @@
 
 use async_sqlite::{Pool, PoolBuilder, JournalMode};
 use async_trait::async_trait;
-use log::{error, info};
+use log::{debug, error, info};
 
 use crate::Result;
 use crate::errors::Error;
@@ -24,6 +24,19 @@ impl SqliteDatabase {
     pub async fn new() -> Result<Self> {
         let path = get_sqlite_path()?;
 
+        Ok(Self {
+            pool: Self::create_pool(&path).await?,
+        })
+    }
+
+    async fn create_pool(path: &str) -> Result<Pool> {
+        match path {
+            ":memory:" => Self::create_memory_pool().await,
+            _ => Self::create_file_pool(path).await,
+        }
+    }
+
+    async fn create_file_pool(path: &str) -> Result<Pool> {
         info!("Using SQLite database from {}.", path);
 
         let pool = match PoolBuilder::new().path(path).journal_mode(JournalMode::Wal).open().await {
@@ -34,9 +47,60 @@ impl SqliteDatabase {
             },
         };
 
-        Ok(Self {
-            pool,
-        })
+        Ok(pool)
+    }
+
+    async fn create_memory_pool() -> Result<Pool> {
+        info!("Using an in-memory SQLite database.");
+
+        let pool = match PoolBuilder::new().num_conns(1).open().await {
+            Ok(value) => value,
+            Err(e) => {
+                error!("Error connecting to the database: {}", e);
+                return Err(Error::DatabaseConnect);
+            },
+        };
+
+        Self::setup_memory_db(&pool).await?;
+
+        Ok(pool)
+    }
+
+    async fn setup_memory_db(pool: &Pool) -> Result<()> {
+        let script = include_str!("../../../dev/schema-sqlite.sql");
+
+        let res = pool.conn_mut(move |conn| {
+            let tx = conn.transaction()?;
+            tx.execute_batch(script)?;
+            tx.commit()
+        }).await;
+
+        let _ = match res {
+            Ok(_) => Ok(()),
+            Err(e) => {
+                error!("Error setting up in-memory database: {}", e);
+                Err(Error::DatabaseQuery)
+            },
+        };
+
+        debug!("Memory database initialized.");
+
+        Ok(())
+    }
+
+    #[allow(unused)]
+    async fn execute(&self, script: String) -> Result<()> {
+        let res = self.pool.conn(move |conn| {
+            conn.execute(&script, [])
+        }).await;
+
+        match res {
+            Ok(_) => Ok(()),
+            Err(e) => {
+                error!("Error executing SQL script: {}", e);
+                Err(Error::DatabaseQuery)
+            },
+        }
     }
 }
 
@@ -48,7 +112,7 @@ impl Database for SqliteDatabase {
      * https://docs.rs/rusqlite/0.30.0/rusqlite/index.html
      */
     async fn get_trees(&self, bounds: Bounds) -> Result<TreeList> {
-        let res = self.pool.conn(move |conn| {
+        let trees = self.pool.conn(move |conn| {
             let mut stmt = conn.prepare("SELECT id, lat, lon FROM trees WHERE lat <= ? AND lat >= ? AND lon <= ? AND lon >= ?")?;
             let mut rows = stmt.query([bounds.n, bounds.s, bounds.e, bounds.w])?;
 
@@ -67,16 +131,7 @@ impl Database for SqliteDatabase {
             }
 
             Ok(trees)
-        }).await;
-
-        let trees = match res {
-            Ok(value) => value,
-
-            Err(e) => {
-                error!("Error reading trees from the database: {}", e);
-                return Err(Error::DatabaseQuery);
-            },
-        };
+        }).await?;
 
         Ok(TreeList {
             trees,
@@ -89,5 +144,40 @@ impl Clone for SqliteDatabase {
         Self {
             pool: self.pool.clone(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env;
+    use env_logger;
+
+    #[tokio::test]
+    async fn test_get_trees() {
+        env::set_var("RUST_LOG", "debug");
+        env::set_var("TREEMAP_SQLITE_PATH", ":memory:");
+
+        env_logger::init();
+
+        let db = SqliteDatabase::new().await.unwrap();
+        db.execute(include_str!("./fixtures/trees.sql").to_string()).await.unwrap();
+
+        let bounds = Bounds {
+            n: 90.0,
+            e: 180.0,
+            s: -90.0,
+            w: -180.0,
+        };
+
+        let trees = match db.get_trees(bounds).await {
+            Ok(value) => value,
+
+            Err(e) => {
+                panic!("Error reading trees from the database: {}", e);
+            },
+        };
+
+        assert_eq!(trees.trees.len(), 1);
     }
 }
