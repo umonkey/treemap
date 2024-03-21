8692a4dbc952d99fc4cecc63c089e1559eb16e2d
Justin Forest <hex@umonkey.net>
2024-03-21 22:25:07 +0400

    Update SQLite schema

--- backend/dev/schema-sqlite.sql
+++ backend/dev/schema-sqlite.sql
@@ -2,9 +2,39 @@
 -- Use `make sqlite-schema` to apply.
 
 CREATE TABLE IF NOT EXISTS trees (
-    `id` INT NOT NULL,   -- snowflake
+    `id` INT NOT NULL,
     `lat` REAL NOT NULL,
     `lon` REAL NOT NULL,
     `name` TEXT NOT NULL,
     PRIMARY KEY(`id`)
 );
+
+
+-- Tree attributes.
+-- This contains random properties, like year of planting, height, etc.
+--
+-- We keep all records ever added, we then aggregate them to use the latest
+-- added value for each attribute to show on the tree page.
+CREATE TABLE IF NOT EXISTS trees_props (
+    `id` INT NOT NULL,
+    `tree_id` INT NOT NULL,
+    `added_at` INT NOT NULL,
+    `name` TEXT NOT NULL,
+    `value` TEXT NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+CREATE INDEX IF NOT EXISTS trees_props_tree_id ON trees_props (tree_id);
+
+
+-- Tree images.
+CREATE TABLE IF NOT EXISTS trees_files (
+    `id` INT NOT NULL,
+    `tree_id` INT NOT NULL,
+    `added_at` INT NOT NULL,
+    `small_url` TEXT NOT NULL,
+    `large_url` TEXT NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+CREATE INDEX IF NOT EXISTS trees_files_tree_id ON trees_files (tree_id);
