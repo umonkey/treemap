0caf50289f5746ae4ec4d4016c2861a778aff60a
Justin Forest <hex@umonkey.net>
2024-03-22 14:20:11 +0400

    Remove API prefix from Storybook

--- frontend/src/utils/env.ts
+++ frontend/src/utils/env.ts
@@ -1,6 +1,11 @@
 import { DEFAULT_API_ROOT } from "@/utils/config";
 
 export const getApiRoot = () => {
+  // Have unprefixed requests in Storybook, for simpler mocking.
+  if (import.meta.env.STORYBOOK === "true") {
+    return "";
+  }
+
   const root = import.meta.env.VITE_API_ROOT || DEFAULT_API_ROOT;
   console.debug(`API Root is ${root}`);
   return root;
