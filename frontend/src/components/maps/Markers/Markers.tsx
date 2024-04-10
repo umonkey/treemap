501789bb48d412944b7f1a90bbc74acdc93e0e6a
Justin Forest <hex@umonkey.net>
2024-04-11 01:58:45 +0400

    Add links to tree pages

--- frontend/src/components/maps/Markers/Markers.tsx
+++ frontend/src/components/maps/Markers/Markers.tsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
 import { Marker, Popup } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 
 import { GreenCircleIcon } from "@/components";
 import { ITreeInfo } from "@/types";
+import { routes } from "@/utils/routes";
 
 interface IProps {
   markers: ITreeInfo[];
@@ -11,7 +13,7 @@ interface IProps {
 export const Markers = (props: IProps) => {
   const markers = props.markers?.map((marker: ITreeInfo, index: number) => (
     <Marker key={index} position={[marker.lat, marker.lon]} icon={GreenCircleIcon}>
-      <Popup>{marker.name}</Popup>
+      <Popup><Link to={routes.treeDetails(marker.id.toString())}>{marker.name}</Link></Popup>
     </Marker>
   ));
 
--- frontend/src/utils/routes.ts
+++ frontend/src/utils/routes.ts
@@ -3,4 +3,5 @@ import { ILatLng } from "@/types";
 export const routes = {
   home: () => "/",
   addTree: (position: ILatLng) => `/add?lat=${position.lat}&lon=${position.lon}`,
+  treeDetails: (id: string) => `/tree/${id}`,
 };
