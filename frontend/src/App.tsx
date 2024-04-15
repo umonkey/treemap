f7747a50e72eafe59a8d5e67d834d6d2560d56c5
Justin Forest <hex@umonkey.net>
2024-04-15 14:51:48 +0400

    Change font to Roboto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,9 @@ import { HashRouter } from "react-router-dom";
 import { RouterComponent } from "@/components";
 
 import "./App.scss";
+import "@fontsource/roboto";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
   return (
