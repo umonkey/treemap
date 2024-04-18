ac09f77e32f2545606ce8ce1ed60540b38b2ba96
Justin Forest <hex@umonkey.net>
2024-04-18 17:33:52 +0400

    Round tree metrics

---
+++ frontend/src/components/maps/TreePopup/utils.test.ts
@@ -0,0 +1,26 @@
+import { formatMeta, formatState } from "./utils";
+
+test("should round numbers", () => {
+  const meta = formatMeta({
+    id: "1",
+    lat: 0,
+    lon: 0,
+    name: "Tree",
+    height: 1.234,
+    circumference: 2.345,
+    diameter: 3.456,
+    state: "healthy",
+    updated_at: 0,
+    thumbnail_id: null,
+  });
+
+  expect(meta).toBe("H= 1.2 m, C= 2.35 m, D= 3.5 m");
+});
+
+test("should format state", () => {
+  expect(formatState("healthy")).toBe("Looks good.");
+  expect(formatState("sick")).toBe("Looks sick.");
+  expect(formatState("dead")).toBe("Looks dead.");
+  expect(formatState("deformed")).toBe("Looks deformed.");
+  expect(formatState("gone")).toBe("Was removed completely.");
+});
--- frontend/src/components/maps/TreePopup/utils.ts
+++ frontend/src/components/maps/TreePopup/utils.ts
@@ -16,15 +16,15 @@ export const formatMeta = (tree: ITreeDetails): string => {
   const parts = [];
 
   if (tree.height) {
-    parts.push(`H= ${tree.height} m`);
+    parts.push(`H= ${tree.height.toFixed(1)} m`);
   }
 
   if (tree.circumference) {
-    parts.push(`C= ${tree.circumference} m`);
+    parts.push(`C= ${tree.circumference.toFixed(2)} m`);
   }
 
   if (tree.diameter) {
-    parts.push(`D= ${tree.diameter} m`);
+    parts.push(`D= ${tree.diameter.toFixed(1)} m`);
   }
 
   return parts.join(", ");
