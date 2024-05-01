ff0044a30c828d432252f159e28054af7fbfb6a9
Justin Forest <hex@umonkey.net>
2024-05-02 01:00:21 +0400

    Move the add details page to a separate URL

--- frontend/src/components/router/RouterComponent/RouterComponent.tsx
+++ frontend/src/components/router/RouterComponent/RouterComponent.tsx
@@ -7,7 +7,7 @@ export const RouterComponent = () => {
   return (
     <Routes>
       <Route path={routes.home()} element={<HomePage />} />
-      <Route path="/add" element={<AddTreePageWrapper />} />
+      <Route path="/add/continue" element={<AddTreePageWrapper />} />
       <Route path={routes.treeDetails(":id")} element={<DetailsPageWrapper />} />
       <Route path={routes.editTree(":id")} element={<EditTreePageWrapper />} />
       <Route path={routes.moveTree(":id")} element={<MoveTreePageWrapper />} />
--- frontend/src/utils/routes.ts
+++ frontend/src/utils/routes.ts
@@ -3,7 +3,7 @@ import { ILatLng } from "@/types";
 export const routes = {
   home: () => "/",
   addComment: (tree_id: string) => `/tree/${tree_id}/comment`,
-  addTree: (position: ILatLng) => `/add?lat=${position.lat}&lon=${position.lon}`,
+  addTree: (position: ILatLng) => `/add/continue?lat=${position.lat}&lon=${position.lon}`,
   treeDetails: (id: string) => `/tree/${id}`,
   editTree: (id: string) => `/tree/${id}/edit`,
   moveTree: (id: string) => `/tree/${id}/move`,
