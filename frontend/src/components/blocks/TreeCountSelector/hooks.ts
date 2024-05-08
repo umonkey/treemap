74d72c6a768965fe3efd0a10347b12ef90c0f148
Justin Forest <hex@umonkey.net>
2024-05-08 14:13:18 +0400

    Add tree count selector UI

---
+++ frontend/src/components/blocks/TreeCountSelector/hooks.ts
@@ -0,0 +1,45 @@
+// Global imports.
+import { useEffect, useState } from "react";
+
+interface IProps {
+  onChange: (value: number) => void;
+}
+
+export const useTreeCountSelector = (props: IProps) => {
+  const [mode, setMode] = useState<string>("single");
+  const [number, setNumber] = useState<number>(2);
+
+  useEffect(() => {
+    props.onChange(number);
+  }, [number, props]);
+
+  useEffect(() => {
+    if (mode === "single") {
+      props.onChange(1);
+    } else {
+      props.onChange(number);
+    }
+  }, [mode, number, props]);
+
+  const handleModeChange = (
+    // @ts-expect-error TS6133
+    event: React.MouseEvent<HTMLElement>,
+    value: string | null,
+  ) => {
+    value && setMode(value);
+  };
+
+  const handleNumberChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setNumber(parseInt(event.target.value || "2"));
+  };
+
+  return {
+    mode,
+    number,
+    handleModeChange,
+    handleNumberChange,
+    singleLabel: mode === "single" ? "Single tree" : "Single",
+  };
+};
---
+++ frontend/src/components/blocks/TreeCountSelector/index.ts
@@ -0,0 +1,1 @@
+export { TreeCountSelector } from "./TreeCountSelector";
