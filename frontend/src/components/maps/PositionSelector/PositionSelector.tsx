a7cfe97dc261aa4626393460deb6f9b9df63305f
Justin Forest <hex@umonkey.net>
2024-05-08 15:07:50 +0400

    Add tree/row selector UI

---
+++ frontend/src/components/maps/PositionSelector/PositionSelector.tsx
@@ -0,0 +1,58 @@
+/**
+ * A complex UI element to select one or multiple positions.
+ *
+ * Shows a map, lets the user select one or multiple points,
+ * reports them via the callback.  More UI features can be added later.
+ */
+
+// Project imports.
+import { Marker } from "react-leaflet";
+import { GreenCircleIcon, LocationPicker, MapBase, RowSelector, TreeCountSelector } from "@/components";
+
+// Local imports.
+import { usePositionSelector } from "./hooks";
+import { IProps } from "./types";
+import "./styles.scss";
+
+export const PositionSelector = (props: IProps) => {
+  const {
+    dots,
+    handleNumberChange,
+    handleRowChange,
+    handleSinglePositionChange,
+    mapState,
+    number,
+  } = usePositionSelector(props);
+
+  return (
+    <div className="PositionSelector">
+      <TreeCountSelector
+        onChange={handleNumberChange}
+      />
+
+      <MapBase center={mapState.center} zoom={mapState.zoom}>
+        {number === 1 && (
+          <LocationPicker
+            onChange={handleSinglePositionChange}
+          />
+        )}
+
+        {number > 1 && (
+          <RowSelector
+            center={mapState.center}
+            onChange={handleRowChange}
+          />
+        )}
+
+        {dots.map((dot, index) => (
+          <Marker
+            key={index}
+            position={[dot.lat, dot.lon]}
+            icon={GreenCircleIcon}
+          />
+        ))}
+      </MapBase>
+
+    </div>
+  );
+};
