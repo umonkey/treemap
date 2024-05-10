c4331d43ae0588808583eb3a9859488758cb54cf
Justin Forest <hex@umonkey.net>
2024-05-10 21:24:15 +0400

    Fix sharing

--- frontend/src/components/toolbar/ShareToolbarButton/hooks.ts
+++ frontend/src/components/toolbar/ShareToolbarButton/hooks.ts
@@ -1,14 +1,27 @@
-import { useNavigate } from "react-router-dom";
+// Global imports.
+import { toast } from "react-hot-toast";
 
-import { routes } from "@/utils";
-
+// Local imports.
 import { IProps } from "./types";
 
 export const useShareToolbarButton = (props: IProps) => {
-  const navigate = useNavigate();
+  const handleClick = async () => {
+    console.debug(`Sharing tree ${props.id}`);
 
-  const handleClick = () => {
-    navigate(routes.editTree(props.id));
+    if (!navigator.share) {
+      toast.error("Your browser does not support sharing.");
+      return;
+    }
+
+    try {
+      await navigator.share({
+        title: "Check out this tree on the Tree Map!",
+        url: window.location.href,
+      });
+    } catch (e) {
+      console.error("Error sharing tree", e);
+      toast.error("Error sharing tree.");
+    }
   };
 
   return {
