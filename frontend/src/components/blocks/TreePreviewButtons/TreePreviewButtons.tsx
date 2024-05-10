c39a4144bb0ec0330cff10af679dd5ed5387f28e
Justin Forest <hex@umonkey.net>
2024-05-10 13:55:12 +0400

    Add photo button to tree previews

--- frontend/src/components/blocks/TreePreviewButtons/TreePreviewButtons.tsx
+++ frontend/src/components/blocks/TreePreviewButtons/TreePreviewButtons.tsx
@@ -1,23 +1,37 @@
+// Global imports.
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, ButtonGroup } from "@mui/material";
-import { faPencil, faUpDownLeftRight } from "@fortawesome/free-solid-svg-icons";
+import { faCamera, faPencil, faUpDownLeftRight } from "@fortawesome/free-solid-svg-icons";
 
+// Project imports.
+import { ImagePicker } from "@/components";
+
+// Local imports.
 import { useTreePreviewButtons } from "./hooks";
+import "./styles.scss";
 
 interface IProps {
   id: string;
 }
 
 export const TreePreviewButtons = (props: IProps) => {
-  const { handleDetailsClick, handleEditClick, handleMoveClick } = useTreePreviewButtons(props.id);
+  const {
+    handleDetailsClick,
+    handleEditClick,
+    handleImageUpload,
+    handleMoveClick,
+  } = useTreePreviewButtons(props.id);
 
   return (
-    <div className="buttons">
+    <div className="TreePreviewButtons buttons">
       <ButtonGroup variant="contained">
         <Button onClick={handleDetailsClick}>Details</Button>
         <Button onClick={handleEditClick}>
           <FontAwesomeIcon icon={faPencil} />
         </Button>
+        <ImagePicker onChange={handleImageUpload}>
+          <FontAwesomeIcon icon={faCamera} />
+        </ImagePicker>
         <Button onClick={handleMoveClick}>
           <FontAwesomeIcon icon={faUpDownLeftRight} />
         </Button>
--- frontend/src/components/blocks/TreePreviewButtons/hooks.ts
+++ frontend/src/components/blocks/TreePreviewButtons/hooks.ts
@@ -3,9 +3,11 @@ import { useNavigate } from "react-router-dom";
 
 // Project imports.
 import { routes } from "@/utils";
+import { useFileUploader } from "@/hooks";
 
 export const useTreePreviewButtons = (id: string) => {
   const navigate = useNavigate();
+  const { uploadFiles } = useFileUploader();
 
   const handleDetailsClick = () => {
     navigate(routes.treeDetails(id));
@@ -19,9 +21,14 @@ export const useTreePreviewButtons = (id: string) => {
     navigate(routes.moveTree(id));
   };
 
+  const handleImageUpload = (files: FileList) => {
+    uploadFiles(id, files);
+  };
+
   return {
     handleDetailsClick,
     handleEditClick,
+    handleImageUpload,
     handleMoveClick,
   };
 };
