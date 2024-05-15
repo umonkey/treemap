dc2ddb2d73689d6a0faf598771b4f90972c15df9
Justin Forest <hex@umonkey.net>
2024-05-15 20:24:15 +0400

    Add more headers to control cache

--- backend/src/actions/get_file.rs
+++ backend/src/actions/get_file.rs
@@ -1,6 +1,7 @@
-use actix_web::http::header::{CacheControl, CacheDirective};
+use actix_web::http::header::{CacheControl, CacheDirective, ETag, EntityTag, Expires};
 use actix_web::{get, web::Data, web::Path, HttpResponse};
 use serde::Deserialize;
+use std::time::{Duration, SystemTime};
 
 use crate::services::AppState;
 use crate::types::Result;
@@ -14,12 +15,21 @@ pub struct PathInfo {
 pub async fn get_file(state: Data<AppState>, path: Path<PathInfo>) -> Result<HttpResponse> {
     let file = state.get_file(path.id).await?;
 
+    let etag = ETag(EntityTag::new_strong(path.id.to_string()));
+
+    let cache_control = CacheControl(vec![
+        CacheDirective::Public,
+        CacheDirective::MaxAge(31536000),
+    ]);
+
+    let expiration = SystemTime::now() + Duration::from_secs(31536000); // 1 year
+    let expiration_header = Expires(expiration.into());
+
     let res = HttpResponse::Ok()
         .content_type("image/jpeg")
-        .insert_header(CacheControl(vec![
-            CacheDirective::Public,
-            CacheDirective::MaxAge(31536000),
-        ]))
+        .insert_header(cache_control)
+        .insert_header(etag)
+        .insert_header(expiration_header)
         .body(file);
 
     Ok(res)
