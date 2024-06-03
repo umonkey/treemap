d679a00518d907b288a5547110978c3ab5b05f27
Justin Forest <hex@umonkey.net>
2024-06-03 11:26:48 +0400

    Handle panic while decoding images

--- backend/src/services/thumbnailer/mod.rs
+++ backend/src/services/thumbnailer/mod.rs
@@ -4,6 +4,7 @@
 use image::{imageops, io::Reader, DynamicImage};
 use log::{debug, error};
 use std::io::Cursor;
+use std::panic;
 
 use crate::types::{Error, Result};
 
@@ -26,16 +27,26 @@ impl ThumbnailerService {
             Err(e) => {
                 error!("Error guessing image format: {:?}", e);
                 return Err(Error::BadImage);
-            },
+            }
         };
 
         debug!("Decoding image.");
-        let img = match format.decode() {
+
+        let v1 = match panic::catch_unwind(|| format.decode()) {
+            Ok(value) => value,
+
+            Err(e) => {
+                error!("Panic during decoding an image: {:?}.", e);
+                return Err(Error::BadImage);
+            }
+        };
+
+        let img = match v1 {
             Ok(value) => value,
             Err(e) => {
                 error!("Error decoding image: {:?}", e);
                 return Err(Error::BadImage);
-            },
+            }
         };
 
         debug!("Image read, size is {}x{}", img.width(), img.height());
@@ -187,17 +198,13 @@ mod tests {
     }
 
     #[test]
-    fn test_broken() {
+    fn test_broken_image() {
         let thumbnailer = setup();
 
         let data = include_bytes!("test/broken.jpg");
         let resized = thumbnailer
-            .resize(&data.to_vec(), 100)
-            .expect("Error resizing image.");
+            .resize(&data.to_vec(), 100);
 
-        let image = read_image(resized);
-
-        assert_eq!(image.width(), 45);
-        assert_eq!(image.height(), 100);
+        assert!(resized.is_err());
     }
 }
