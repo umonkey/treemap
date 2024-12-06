6f729fc4080b655170607646aa600d7d3d9a94a9
Justin Forest <hex@umonkey.net>
2024-12-06 23:20:25 +0400

    Experimental new UI

---
+++ frontend-v2/src/routes/tree/[id]/components/Links.svelte
@@ -0,0 +1,14 @@
+<ul class="links">
+    <li><a href="https://example.com/">OSM</a></li>
+    <li><a href="https://example.com/">Wikipedia</a></li>
+    <li><a href="https://example.com/">Wikidata</a></li>
+</ul>
+
+<style>
+.links {
+    display: flex;
+    gap: 20px;
+    list-style: none;
+    padding: 0;
+}
+</style>
