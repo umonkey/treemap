cd7ed0048dcd50e9bad50354d6441cf8a1fec541
Justin Forest <hex@umonkey.net>
2024-12-06 23:20:25 +0400

    Experimental new UI

---
+++ frontend-v2/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    let { children } = $props();
+</script>
+
+<style>
+:global {
+    body {
+        background-color: #14191f;
+        color: rgb(241, 243, 245);
+        font-family: InterVariable, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Liberation Sans", Helvetica, Arial, sans-serif;
+    }
+
+    a {
+        color: rgb(32, 139, 254);
+    }
+}
+</style>
+
+{@render children()}
---
+++ frontend-v2/src/routes/tree/[id]/components/Properties.svelte
@@ -0,0 +1,53 @@
+<div class="properties">
+    <dl>
+        <dt>Height</dt>
+        <dd>17 m</dd>
+    </dl>
+    <dl>
+        <dt>Canopy</dt>
+        <dd>4.5 m</dd>
+    </dl>
+    <dl>
+        <dt>Circumference</dt>
+        <dd>4.5 m</dd>
+    </dl>
+    <dl>
+        <dt>State</dt>
+        <dd>sick, deformed</dd>
+    </dl>
+    <dl>
+        <dt>Year</dt>
+        <dd>unknown</dd>
+    </dl>
+</div>
+
+<style>
+.properties {
+    columns: 2;
+    gap: 20px;
+    margin: 20px 0;
+}
+
+dl {
+    display: flex;
+    flex-direction: row;
+    margin: 0 0 5px;
+}
+
+dt {
+    flex-shrink: 1;
+    flex-grow: 0;
+}
+
+dd {
+    flex-basis: 50%;
+    flex-shring: 1;
+    flex-grow: 1;
+    text-align: right;
+    margin: 0;
+}
+
+dd {
+    white-space: nowrap;
+}
+</style>
