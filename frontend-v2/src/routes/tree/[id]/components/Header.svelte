54105ed58e1ae7959d02ce5b7be13300484d8167
Justin Forest <hex@umonkey.net>
2024-12-07 12:18:11 +0400

    Update spacings

--- frontend-v2/src/routes/tree/[id]/components/Header.svelte
+++ frontend-v2/src/routes/tree/[id]/components/Header.svelte
@@ -1,5 +1,9 @@
+<script>
+    import BackIcon from '$lib/icons/BackIcon.svelte';
+</script>
+
 <div class="header">
-    <div class="icon">&lt;</div>
+    <div class="icon"><div><BackIcon /></div></div>
     <div>Tree</div>
     <div class="icon"></div>
 </div>
@@ -17,6 +21,15 @@
         flex-basis: 30px;
         flex-grow: 0;
         flex-shrink: 0;
+        height: 30px;
+        align-content: center;
+
+        & > div {
+            width: 20px;
+            height: 20px;
+            padding: 5px;
+            cursor: pointer;
+        }
     }
 }
 </style>
