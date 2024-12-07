fbdbd00d111559a0c05b433a6dedeae014c4d072
Justin Forest <hex@umonkey.net>
2024-12-07 15:17:49 +0400

    Fix formatting

--- frontend-v2/src/routes/tree/[id]/components/Description.svelte
+++ frontend-v2/src/routes/tree/[id]/components/Description.svelte
@@ -1,10 +1,21 @@
 <div class="description">
-    <p>The General Sherman tree was named after the American Civil War general William Tecumseh Sherman. The official story, which may be apocryphal, claims the tree was named in 1879 by naturalist James Wolverton, who had served as a lieutenant in the 9th Indiana Cavalry under Sherman.</p>
-    <p>Генерал Шерман (англ. General Sherman) — экземпляр секвойядендрона гигантского (Sequoiadendron giganteum), растущий в «Гигантском лесу» национального парка «Секвойя» в Калифорнии, США. Первое по объёму и массе дерево на Земле. Высота «Генерала Шермана» — 85 м (по состоянию на 2018 год), масса оценивается в 1910 т (оценка 1938 года), объём ствола — в 1487 м³, возраст — в 2300—2700 лет.</p>
+	<p>
+		The General Sherman tree was named after the American Civil War general William Tecumseh
+		Sherman. The official story, which may be apocryphal, claims the tree was named in 1879 by
+		naturalist James Wolverton, who had served as a lieutenant in the 9th Indiana Cavalry under
+		Sherman.
+	</p>
+	<p>
+		Генерал Шерман (англ. General Sherman) — экземпляр секвойядендрона гигантского (Sequoiadendron
+		giganteum), растущий в «Гигантском лесу» национального парка «Секвойя» в Калифорнии, США. Первое
+		по объёму и массе дерево на Земле. Высота «Генерала Шермана» — 85 м (по состоянию на 2018 год),
+		масса оценивается в 1910 т (оценка 1938 года), объём ствола — в 1487 м³, возраст — в 2300—2700
+		лет.
+	</p>
 </div>
 
 <style>
-    .description {
-        padding: 0 var(--gap);
-    }
+	.description {
+		padding: 0 var(--gap);
+	}
 </style>
--- frontend-v2/src/routes/tree/[id]/components/Links.svelte
+++ frontend-v2/src/routes/tree/[id]/components/Links.svelte
@@ -1,14 +1,14 @@
 <ul class="links">
-    <li><a href="https://example.com/">Wikipedia</a></li>
-    <li><a href="https://example.com/">Wikidata</a></li>
-    <li><a href="https://example.com/">OSM</a></li>
+	<li><a href="https://example.com/">Wikipedia</a></li>
+	<li><a href="https://example.com/">Wikidata</a></li>
+	<li><a href="https://example.com/">OSM</a></li>
 </ul>
 
 <style>
-.links {
-    display: flex;
-    gap: var(--gap);
-    list-style: none;
-    padding: 0 var(--gap);
-}
+	.links {
+		display: flex;
+		gap: var(--gap);
+		list-style: none;
+		padding: 0 var(--gap);
+	}
 </style>
