e2afc94ad9b7e3e46d96464e349b8d1468fc3de5
Justin Forest <hex@umonkey.net>
2024-12-07 20:32:45 +0400

    Add other dummy tabs

---
+++ frontend-v2/src/routes/tree/[id=treeid]/map/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Header from '$lib/components/tree/Header.svelte';
+	import Tabs from '$lib/components/tree/Tabs.svelte';
+	import Title from '$lib/components/tree/Title.svelte';
+
+	const { data } = $props();
+	const tree = data.tree;
+</script>
+
+<svelte:head>
+	<title>{tree.species} - Details</title>
+</svelte:head>
+
+<Header />
+<Title title={tree.species} />
+<Tabs tree={tree.id} active="map" />
+<p>Here will be the map with the tree pinned.</p>
