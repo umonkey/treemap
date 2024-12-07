bddc2cc2ebb3fa5ba737829119c7afcc33c26b17
Justin Forest <hex@umonkey.net>
2024-12-07 20:22:30 +0400

    Fix dynamic loading

--- frontend-v2/src/lib/types.ts
+++ frontend-v2/src/lib/types.ts
@@ -1,3 +1,11 @@
+export interface ITreeFile {
+	id: string;
+	small_id: string;
+	large_id: string;
+	added_at: number | null;
+	added_by: string | null;
+}
+
 export interface ITree {
 	id: string;
 	lat: number;
@@ -5,4 +13,12 @@ export interface ITree {
 	osm_id: number | null;
 	species: string;
 	notes: string | null;
+	height: number | null;
+	circumference: number | null;
+	diameter: number | null;
+	state: string;
+	added_at: number;
+	updated_at: number;
+	added_by: string;
+	files: ITreeFile[];
 }
