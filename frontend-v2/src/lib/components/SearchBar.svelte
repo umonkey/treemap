5abafb6e5df4a45fd33803329a84356722664ea3
Justin Forest <hex@umonkey.net>
2024-12-08 02:22:14 +0400

    Add a dummy search page with examples

---
+++ frontend-v2/src/lib/components/SearchBar.svelte
@@ -0,0 +1,46 @@
+<script>
+	import SearchIcon from '$lib/icons/SearchIcon.svelte';
+</script>
+
+<div class="search">
+	<div class="icon"><SearchIcon width="20px" height="20px" /></div>
+	<input type="search" placeholder="Search" />
+</div>
+
+<style>
+	div.search {
+		background-color: var(--form-background);
+		display: flex;
+		flex-direction: row;
+		height: 40px;
+		border-radius: 8px;
+	}
+
+	.icon {
+		height: 40px;
+		width: 40px;
+		align-content: center;
+		text-align: center;
+		color: var(--icon-color-secondary);
+		padding-top: 4px;
+		box-sizing: border-box;
+
+		flex-basis: 40px;
+		flex-shrink: 0;
+		flex-grow: 0;
+	}
+
+	input {
+		flex-grow: 1;
+
+		background-color: transparent;
+		border: none;
+		color: #fff;
+		height: 40px;
+		line-height: 40px;
+
+		&:focus {
+			outline: none;
+		}
+	}
+</style>
