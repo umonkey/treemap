cc64f44b1063776752dd4fd6dc5bd8168c896c4f
Justin Forest <hex@umonkey.net>
2024-12-07 11:13:42 +0400

    Add new tree page

---
+++ frontend-v2/src/routes/tree/[id]/components/Title.svelte
@@ -0,0 +1,22 @@
+<script type="ts">
+    import DotsIcon from '$lib/icons/DotsIcon.svelte';
+</script>
+
+<div class="title">
+    <h1>Quercus rubra &middot; Manukyan str</h1>
+    <div class="icon">
+        <DotsIcon />
+    </div>
+</div>
+
+<style>
+.title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    h1 {
+        font-size: 1rem;
+    }
+}
+</style>
