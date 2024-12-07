f841585b4847896a596bb29a624778f4d7c10178
Justin Forest <hex@umonkey.net>
2024-12-07 12:18:11 +0400

    Update spacings

--- frontend-v2/src/routes/tree/[id]/components/Actions.svelte
+++ frontend-v2/src/routes/tree/[id]/components/Actions.svelte
@@ -3,21 +3,35 @@
     import ChatIcon from '$lib/icons/ChatIcon.svelte';
     import ShareIcon from '$lib/icons/ShareIcon.svelte';
     import SaveIcon from '$lib/icons/SaveIcon.svelte';
+    import MapIcon from '$lib/icons/MapIcon.svelte';
 </script>
 
 <div class="actions">
-    <div><HeartIcon /></div>
-    <div><ChatIcon /></div>
-    <div>(map)</div>
-    <div><ShareIcon /></div>
-    <div><SaveIcon /></div>
+    <div><HeartIcon class="icon" /></div>
+    <div><ChatIcon class="icon" /></div>
+    <div><MapIcon class="icon" /></div>
+    <div><ShareIcon class="icon" /></div>
+    <div class="sep"></div>
+    <div><SaveIcon class="icon" /></div>
 </div>
 
 <style>
 .actions {
     display: flex;
-    gap: var(--gap);
+    gap: calc(var(--gap) * 1.5);
     justify-content: left;
     margin: var(--gap) 0;
+    padding: 0 var(--gap);
+
+    & > div {
+        width: 24px;
+        height: 24px;
+        color: var(--color-text);
+        text-align: center;
+    }
+
+    .sep {
+        flex-grow: 1;
+    }
 }
 </style>
