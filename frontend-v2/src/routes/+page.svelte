8d2899e80cc2bce5ef7a69a6fbab5d7ba1f24210
Justin Forest <hex@umonkey.net>
2024-12-08 01:18:45 +0400

    Add basic home page

--- frontend-v2/src/routes/+page.svelte
+++ frontend-v2/src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+	<title>Trees of Yerevan</title>
+</svelte:head>
+
+<div class="home">
+	<h1>Tree Map of Yerevan</h1>
+	<p>
+		Welcome to the map of trees in Yerevan. This is a community driven application that everyone can
+		use and update.
+	</p>
+	<p>This page will contain some basic stats as long as a list of recent updates.</p>
+	<p>Please use the navigation bar to access areas of the application.</p>
+	<p>
+		You can find the source code <a href="https://github.com/umonkey/treemap/" target="_blank"
+			>here</a
+		>.
+	</p>
+</div>
+
+<style>
+	.home {
+		padding: 0 var(--gap);
+	}
+</style>
