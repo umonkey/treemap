12a9f574f17882b9266a69a77081c57b75215739
Justin Forest <hex@umonkey.net>
2024-12-07 15:17:49 +0400

    Fix formatting

--- frontend-v2/src/routes/tree/[id]/components/Gallery.svelte
+++ frontend-v2/src/routes/tree/[id]/components/Gallery.svelte
@@ -1,83 +1,86 @@
 <div class="gallery">
-    <div class="imgno">2/10</div>
-    <div class="slides">
-        <div>
-            <img src="https://yerevan.treemaps.app/v1/files/157235005582610432.jpg" />
-            <div class="date">29.10.2019 &middot; umonkey You can just change the file extension to .svelte and import an SVG as a normal component.</div>
-        </div>
-        <div><img src="https://yerevan.treemaps.app/v1/files/157234999588950016.jpg" /></div>
-        <div><img src="https://yerevan.treemaps.app/v1/files/157235134158999552.jpg" /></div>
-        <div><img src="https://yerevan.treemaps.app/v1/files/157235145743667200.jpg" /></div>
-        <div><img src="https://yerevan.treemaps.app/v1/files/157235424761352192.jpg" /></div>
-        <div><img src="https://yerevan.treemaps.app/v1/files/157235430486577152.jpg" /></div>
-        <div><img src="https://yerevan.treemaps.app/v1/files/157235418578948096.jpg" /></div>
-        <div><img src="https://yerevan.treemaps.app/v1/files/157235595687628800.jpg" /></div>
-        <div><img src="https://yerevan.treemaps.app/v1/files/157235721479000064.jpg" /></div>
-        <div><img src="https://yerevan.treemaps.app/v1/files/157235709017722880.jpg" /></div>
-    </div>
+	<div class="imgno">2/10</div>
+	<div class="slides">
+		<div>
+			<img src="https://yerevan.treemaps.app/v1/files/157235005582610432.jpg" />
+			<div class="date">
+				29.10.2019 &middot; umonkey You can just change the file extension to .svelte and import an
+				SVG as a normal component.
+			</div>
+		</div>
+		<div><img src="https://yerevan.treemaps.app/v1/files/157234999588950016.jpg" /></div>
+		<div><img src="https://yerevan.treemaps.app/v1/files/157235134158999552.jpg" /></div>
+		<div><img src="https://yerevan.treemaps.app/v1/files/157235145743667200.jpg" /></div>
+		<div><img src="https://yerevan.treemaps.app/v1/files/157235424761352192.jpg" /></div>
+		<div><img src="https://yerevan.treemaps.app/v1/files/157235430486577152.jpg" /></div>
+		<div><img src="https://yerevan.treemaps.app/v1/files/157235418578948096.jpg" /></div>
+		<div><img src="https://yerevan.treemaps.app/v1/files/157235595687628800.jpg" /></div>
+		<div><img src="https://yerevan.treemaps.app/v1/files/157235721479000064.jpg" /></div>
+		<div><img src="https://yerevan.treemaps.app/v1/files/157235709017722880.jpg" /></div>
+	</div>
 </div>
 
 <style>
-.gallery {
-    position: relative;
-}
+	.gallery {
+		position: relative;
+	}
 
-.imgno {
-    position: absolute;
-    right: var(--gap);
-    top: var(--gap);
-    background-color: rgba(0, 0, 0, 0.85);
-    color: #fff;
-    border-radius: 6px;
-    font-size: 14px;
-    padding: 2px 4px;
-    z-index: 2;
-}
+	.imgno {
+		position: absolute;
+		right: var(--gap);
+		top: var(--gap);
+		background-color: rgba(0, 0, 0, 0.85);
+		color: #fff;
+		border-radius: 6px;
+		font-size: 14px;
+		padding: 2px 4px;
+		z-index: 2;
+	}
 
-.slides {
-    display: flex;
-    max-width: 500px;
-    max-height: 500px;
+	.slides {
+		display: flex;
+		max-width: 500px;
+		max-height: 500px;
 
-    overflow-x: auto;
-    overflow-y: hidden;
-    white-space: nowrap;
-    scroll-snap-type: x mandatory;
+		overflow-x: auto;
+		overflow-y: hidden;
+		white-space: nowrap;
+		scroll-snap-type: x mandatory;
 
-    & > div {
-        display: block;
-        position: relative;
+		& > div {
+			display: block;
+			position: relative;
 
-        min-height: 100%;
-        min-width: 100%;
+			min-height: 100%;
+			min-width: 100%;
 
-        aspect-ratio: 1/1;
-        text-align: center;
-        align-content: center;
-        scroll-snap-align: center;
+			aspect-ratio: 1/1;
+			text-align: center;
+			align-content: center;
+			scroll-snap-align: center;
 
-        img {
-            border: none;
-            object-position: center;
-            object-fit: cover;
-            height: 100%;
-            width: 100%;
-        }
-    }
+			img {
+				border: none;
+				object-position: center;
+				object-fit: cover;
+				height: 100%;
+				width: 100%;
+			}
+		}
 
-    .date {
-        position: absolute;
-        bottom: var(--gap);
-        left: var(--gap);
-        background-color: rgba(0, 0, 0, 0.75);
-        color: #fff;
-        font-size: 14px;
-        padding: 2px 4px;
+		.date {
+			position: absolute;
+			bottom: var(--gap);
+			left: var(--gap);
+			background-color: rgba(0, 0, 0, 0.75);
+			color: #fff;
+			font-size: 14px;
+			padding: 2px 4px;
 
-        /* Prevent text overflow in case of really long username */
-        max-width: calc(100% - 2 * var(--gap) - 8px);
-        overflow: hidden;
-        text-overflow: ellipsis;
-    }
-}
+			/* Prevent text overflow in case of really long username */
+			max-width: calc(100% - 2 * var(--gap) - 8px);
+			overflow: hidden;
+			text-overflow: ellipsis;
+		}
+	}
 </style>
--- frontend-v2/src/routes/tree/[id]/components/Title.svelte
+++ frontend-v2/src/routes/tree/[id]/components/Title.svelte
@@ -1,32 +1,32 @@
 <script type="ts">
-    import DotsIcon from '$lib/icons/DotsIcon.svelte';
+	import DotsIcon from '$lib/icons/DotsIcon.svelte';
 </script>
 
 <div class="title">
-    <h1><strong>Quercus rubra</strong> &middot; Manukyan str</h1>
-    <div class="icon">
-        <DotsIcon />
-    </div>
+	<h1><strong>Quercus rubra</strong> &middot; Manukyan str</h1>
+	<div class="icon">
+		<DotsIcon />
+	</div>
 </div>
 
 <style>
-.title {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 50px;
-    padding: 0 var(--gap);
+	.title {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		height: 50px;
+		padding: 0 var(--gap);
 
-    h1 {
-        font-size: 1rem;
-        font-weight: 400;
-        line-height: 50px;
-        margin: 0;
-    }
+		h1 {
+			font-size: 1rem;
+			font-weight: 400;
+			line-height: 50px;
+			margin: 0;
+		}
 
-    .icon {
-        flex-basis: 25px;
-        color: var(--icon-color-secondary);
-    }
-}
+		.icon {
+			flex-basis: 25px;
+			color: var(--icon-color-secondary);
+		}
+	}
 </style>
