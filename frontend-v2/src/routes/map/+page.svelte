165d698bf5ff6ae9bc6c157d5c17bd2bcf314964
Justin Forest <hex@umonkey.net>
2024-12-08 03:13:29 +0400

    Add dummy map page

---
+++ frontend-v2/src/routes/map/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Header from '$lib/components/tree/Header.svelte';
+	import Map from '$lib/components/Map.svelte';
+	import { DEFAULT_MAP_CENTER } from '$lib/constants';
+</script>
+
+<svelte:head>
+	<title>Map</title>
+</svelte:head>
+
+<Header />
+
+<Map center={DEFAULT_MAP_CENTER} />
