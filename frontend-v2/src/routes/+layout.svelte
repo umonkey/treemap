f2354252c55090c222462d8a97196566abc1668f
Justin Forest <hex@umonkey.net>
2024-12-07 15:17:49 +0400

    Fix formatting

--- frontend-v2/src/routes/+layout.svelte
+++ frontend-v2/src/routes/+layout.svelte
@@ -1,56 +1,56 @@
 <script>
-    import '@fontsource-variable/inter';
+	import '@fontsource-variable/inter';
 
-    let { children } = $props();
+	let { children } = $props();
 </script>
 
-<style>
-:global {
-    :root {
-        --gap: 10px;
-
-        font-size: 16px;
-        font-family: 'Inter Variable', sans-serif;
-
-        /* Colors */
-        --background-color: #fff;
-        --icon-color-secondary: #6f869f;
-        --link-color: #1083fe;
-        --sep-color: #d4dbe2;
-        --tab-color: #1083fe;
-        --text-color-inactive: #42576c;
-        --text-color: #0b0f14;
-    }
-
-    body {
-        background-color: var(--background-color);
-        color: var(--text-color);
-        padding: 0;
-        margin: 0;
-    }
-
-    a {
-        color: var(--link-color);
-    }
-
-    p {
-        line-height: 1.5em;
-    }
-}
-
-@media(prefers-color-scheme: dark) {
-    :global {
-        :root {
-            --background-color: #14191f;
-            --icon-color-secondary: #788ea5;
-            --link-color: #208bfe;
-            --sep-color: #2e4052;
-            --tab-color: #208bfe;
-            --text-color-inactive: #aebbc9;
-            --text-color: #f1f3f5;
-        }
-    }
-}
-</style>
-
 {@render children()}
+
+<style>
+	:global {
+		:root {
+			--gap: 10px;
+
+			font-size: 16px;
+			font-family: 'Inter Variable', sans-serif;
+
+			/* Colors */
+			--background-color: #fff;
+			--icon-color-secondary: #6f869f;
+			--link-color: #1083fe;
+			--sep-color: #d4dbe2;
+			--tab-color: #1083fe;
+			--text-color-inactive: #42576c;
+			--text-color: #0b0f14;
+		}
+
+		body {
+			background-color: var(--background-color);
+			color: var(--text-color);
+			padding: 0;
+			margin: 0;
+		}
+
+		a {
+			color: var(--link-color);
+		}
+
+		p {
+			line-height: 1.5em;
+		}
+	}
+
+	@media (prefers-color-scheme: dark) {
+		:global {
+			:root {
+				--background-color: #14191f;
+				--icon-color-secondary: #788ea5;
+				--link-color: #208bfe;
+				--sep-color: #2e4052;
+				--tab-color: #208bfe;
+				--text-color-inactive: #aebbc9;
+				--text-color: #f1f3f5;
+			}
+		}
+	}
+</style>
--- frontend-v2/src/routes/tree/[id]/components/Header.svelte
+++ frontend-v2/src/routes/tree/[id]/components/Header.svelte
@@ -1,36 +1,36 @@
 <script>
-    import BackIcon from '$lib/icons/BackIcon.svelte';
+	import BackIcon from '$lib/icons/BackIcon.svelte';
 </script>
 
 <div class="header">
-    <div class="icon"><div><BackIcon /></div></div>
-    <div>Tree</div>
-    <div class="icon"></div>
+	<div class="icon"><div><BackIcon /></div></div>
+	<div>Tree</div>
+	<div class="icon"></div>
 </div>
 
 <style>
-.header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 var(--gap);
-    border-bottom: 1px solid var(--sep-color);
-    font-weight: 800;
-    height: 40px;
+	.header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 0 var(--gap);
+		border-bottom: 1px solid var(--sep-color);
+		font-weight: 800;
+		height: 40px;
 
-    .icon {
-        flex-basis: 30px;
-        flex-grow: 0;
-        flex-shrink: 0;
-        height: 30px;
-        align-content: center;
+		.icon {
+			flex-basis: 30px;
+			flex-grow: 0;
+			flex-shrink: 0;
+			height: 30px;
+			align-content: center;
 
-        & > div {
-            width: 20px;
-            height: 20px;
-            padding: 5px;
-            cursor: pointer;
-        }
-    }
-}
+			& > div {
+				width: 20px;
+				height: 20px;
+				padding: 5px;
+				cursor: pointer;
+			}
+		}
+	}
 </style>
