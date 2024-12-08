73e6b1e1aa86de7d9434323b6a8efbeace7f35cd
Justin Forest <hex@umonkey.net>
2024-12-09 00:39:35 +0400

    Add dummy tree preview on the map

---
+++ frontend-v2/src/lib/components/map/MapPreview.svelte
@@ -0,0 +1,60 @@
+<script>
+	import CloseIcon from '$lib/icons/CloseIcon.svelte';
+
+	export let tree;
+	export let onClose;
+</script>
+
+{#if tree}
+	<div class="preview">
+		<div class="header">
+			<div class="title">{tree.species}</div>
+			<button class="close" on:click={onClose}><CloseIcon /></button>
+		</div>
+		<div class="props">H=4m D=15m C=120cm</div>
+	</div>
+{/if}
+
+<style>
+	.preview {
+		padding: var(--gap);
+		line-height: 1.5em;
+	}
+
+	.header {
+		display: flex;
+		flex-direction: row;
+
+		.title {
+			flex-grow: 1;
+			flex-shrink: 1;
+		}
+
+		.close {
+			flex-basis: 30px;
+			flex-grow: 0;
+			flex-shrink: 0;
+
+			width: 30px;
+			height: 30px;
+			cursor: pointer;
+		}
+	}
+
+	@media (max-width: 480px) {
+		.preview {
+			position: fixed;
+			bottom: 50px;
+
+			z-index: 5;
+
+			width: 100%;
+			box-sizing: border-box;
+			background-color: #000;
+			border-top-left-radius: 8px;
+			border-top-right-radius: 8px;
+
+			padding: var(--gap);
+		}
+	}
+</style>
