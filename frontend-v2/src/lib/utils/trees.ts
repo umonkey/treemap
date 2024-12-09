af730aa8fb084d974e5f7c2e0cf29276a5c699c7
Justin Forest <hex@umonkey.net>
2024-12-09 14:06:10 +0400

    Show correct tree details in map preview

---
+++ frontend-v2/src/lib/utils/trees.ts
@@ -0,0 +1,25 @@
+import type { ITree } from '$lib/types';
+
+export const shortDetails = (tree: ITree): string => {
+	const parts = [];
+
+	if (tree.height) {
+		parts.push(`H=${tree.height.toFixed(1)}m`);
+	} else {
+		parts.push('H=?');
+	}
+
+	if (tree.diameter) {
+		parts.push(`D=${tree.diameter.toFixed(1)}m`);
+	} else {
+		parts.push('D=?');
+	}
+
+	if (tree.circumference) {
+		parts.push(`C=${(tree.circumference * 100).toFixed(1)}cm`);
+	} else {
+		parts.push('C=?');
+	}
+
+	return parts.join(' ');
+};
