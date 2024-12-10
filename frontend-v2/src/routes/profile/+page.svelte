4bde382da187fe47901e64b54505716d294ffd68
Justin Forest <hex@umonkey.net>
2024-12-10 16:27:18 +0400

    Logging in

---
+++ frontend-v2/src/routes/profile/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { authState } from '$lib/stores/auth';
+	import SignIn from '$lib/components/auth/SignIn.svelte';
+</script>
+
+<h1>Profile</h1>
+
+{#if authState}
+	<p>You are logged in as <em>{$authState.name}</em>.</p>
+{:else}
+	<SignIn />
+{/if}
