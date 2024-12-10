c4ce7a621a5a847976ea579eefe931c61c02a9d5
Justin Forest <hex@umonkey.net>
2024-12-10 12:53:42 +0400

    Persist main map state

--- frontend-v2/src/lib/stores/map.ts
+++ frontend-v2/src/lib/stores/map.ts
@@ -1,9 +1,16 @@
 import { writable, derived } from 'svelte/store';
 import { DEFAULT_MAP_CENTER } from '$lib/constants';
+import { ls } from '$lib/utils/localStorage';
 
-export const mapState = writable({
-	center: DEFAULT_MAP_CENTER,
-	zoom: 15
+export const mapState = writable(
+	ls.read('mapState') || {
+		center: DEFAULT_MAP_CENTER,
+		zoom: 15
+	}
+);
+
+mapState.subscribe((value) => {
+	ls.write('mapState', value);
 });
 
 export const mapCenter = derived(mapState, ($mapState) => $mapState.center);
