54a648865d579d3410e27f2ae66bd24a31ee4569
Justin Forest <hex@umonkey.net>
2024-12-10 21:43:40 +0400

    Add comments

---
+++ frontend-v2/src/lib/components/forms/CommentInput.svelte
@@ -0,0 +1,6 @@
+<script>
+	import TextInput from '$lib/components/forms/TextInput.svelte';
+	export let value = '';
+</script>
+
+<TextInput label="Comment" bind:value multiline={true} hint="Some hints or suggested edits." />
