b36602ec5b2e5325b0bebe2190b5dfed7d444c47
Justin Forest <hex@umonkey.net>
2024-12-11 10:13:08 +0400

    Add basic error template

---
+++ frontend-v2/src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="container">
+	<h1>Error {$page.status}</h1>
+
+	{#if $page.status == 500}
+		<p>We are sorry, but this page is not working currently. Please try again later.</p>
+	{:else}
+		<p>{$page.error.message}</p>
+	{/if}
+</div>
+
+<style>
+	.container {
+		padding: 0 var(--gap);
+	}
+</style>
