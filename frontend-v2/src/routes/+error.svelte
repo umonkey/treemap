bcf171e06bea9d7615966499676f22259d44e6fa
Justin Forest <hex@umonkey.net>
2024-12-11 11:14:45 +0400

    Add a dummy updates page

--- frontend-v2/src/routes/+error.svelte
+++ frontend-v2/src/routes/+error.svelte
@@ -3,11 +3,14 @@
 </script>
 
 <div class="container">
-	<h1>Error {$page.status}</h1>
-
 	{#if $page.status == 500}
+		<h1>Something went wrong</h1>
 		<p>We are sorry, but this page is not working currently. Please try again later.</p>
+	{:else if $page.status == 404}
+		<h1>Page not found</h1>
+		<p>We are sorry, but this page does not exist.</p>
 	{:else}
+		<h1>Error {$page.status}</h1>
 		<p>{$page.error.message}</p>
 	{/if}
 </div>
---
+++ frontend-v2/src/routes/updates/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import AuthWrapper from '$lib/components/auth/AuthWrapper.svelte';
+</script>
+
+<div class="padded">
+	<h1>Recent updates</h1>
+
+	<AuthWrapper>
+		<p>This page is under construction. Please come back later.</p>
+	</AuthWrapper>
+</div>
