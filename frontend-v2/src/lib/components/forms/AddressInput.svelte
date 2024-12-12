e354c45119e1ee7c0703297b1d1bebf7c5c540a8
Justin Forest <hex@umonkey.net>
2024-12-13 00:04:19 +0400

    Add form hints

--- frontend-v2/src/lib/components/forms/AddressInput.svelte
+++ frontend-v2/src/lib/components/forms/AddressInput.svelte
@@ -3,4 +3,4 @@
 	export let value = '';
 </script>
 
-<TextInput label="Address" bind:value />
+<TextInput label="Address" bind:value hint="This is normally set automatically when adding a tree, but you can edit." />
