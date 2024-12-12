4542b0a8a2e7c0c996defd6bf7bec389cf89033a
Justin Forest <hex@umonkey.net>
2024-12-13 00:01:23 +0400

    Add share button

---
+++ frontend-v2/src/lib/components/tree/ShareButton.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { toast } from '@zerodevx/svelte-toast';
+	import ShareIcon from '$lib/icons/ShareIcon.svelte';
+
+	const onClick = async () => {
+		if (!navigator.share) {
+			toast.push("Your browser doesn't support sharing.");
+			return;
+		}
+
+		try {
+			await navigator.share({
+				title: document.title,
+				url: window.location.href
+			});
+		} catch (e) {
+			console.error('Error sharing a tree.', e);
+			toast.push('Error sharing this page.');
+		}
+	};
+</script>
+
+<button type="button" onclick={onClick} title="Share this page">
+	<ShareIcon />
+</button>
+
+<style>
+	button {
+		background: none;
+		border: none;
+		color: inherit;
+		padding: 0;
+		margin: 0;
+		cursor: pointer;
+	}
+</style>
