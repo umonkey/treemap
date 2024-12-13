bdb45ea5028770f0d8c075519f697c1b4633910b
Justin Forest <hex@umonkey.net>
2024-12-13 17:18:56 +0400

    Switch sign in to redirect

--- frontend-v2/src/lib/components/auth/SignIn.svelte
+++ frontend-v2/src/lib/components/auth/SignIn.svelte
@@ -1,7 +1,15 @@
-<script>
+<script lang="ts">
 	import { googleCallbackHandler } from '$lib/utils/auth';
 
 	window.onSignIn = googleCallbackHandler;
+
+	const getUxMode = (): string => {
+		if (window.matchMedia('(display-mode: standalone)').matches) {
+			return 'redirect';
+		}
+
+		return 'popup';
+	};
 </script>
 
 <svelte:head>
@@ -12,7 +20,7 @@
 	id="g_id_onload"
 	data-client_id="999312923392-6k26jala2pe5dk9u7o63o8nvts3a7f1f.apps.googleusercontent.com"
 	data-context="signin"
-	data-ux_mode="popup"
+	data-ux_mode={getUxMode()}
 	data-callback="onSignIn"
 	data-itp_support="true"
 ></div>
