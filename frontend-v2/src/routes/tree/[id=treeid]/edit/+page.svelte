59c9ee88640bf71785da0d6da03fcfccb51d6d0e
Justin Forest <hex@umonkey.net>
2024-12-17 19:26:12 +0400

    Drop address input

--- frontend-v2/src/routes/tree/[id=treeid]/edit/+page.svelte
+++ frontend-v2/src/routes/tree/[id=treeid]/edit/+page.svelte
@@ -4,7 +4,6 @@
 	import { routes } from '$lib/routes';
 	import { toast } from '@zerodevx/svelte-toast';
 
-	import AddressInput from '$lib/components/forms/AddressInput.svelte';
 	import AuthWrapper from '$lib/components/auth/AuthWrapper.svelte';
 	import Button from '$lib/components/forms/Button.svelte';
 	import CanopyInput from '$lib/components/forms/CanopyInput.svelte';
@@ -24,17 +23,8 @@
 	let diameter = $state(data.tree.diameter?.toString() ?? '0');
 	let circumference = $state(data.tree.circumference?.toString() ?? '0');
 	let treeState = $state(data.tree.state ?? '');
-	let address = $state('');
 	let notes = $state(data.tree.notes ?? '');
 	let location = $state([data.tree.lat, data.tree.lon]);
-
-	$effect(() => {
-		console.debug('Species changed to:', species);
-	});
-
-	$effect(() => {
-		console.debug('Address changed to:', address);
-	});
 
 	const onSave = () => {
 		apiClient
@@ -44,7 +34,6 @@
 				diameter: parseFloat(diameter),
 				circumference: parseFloat(circumference),
 				state: treeState,
-				address,
 				notes
 			})
 			.then((res) => {
@@ -80,7 +69,6 @@
 		<CircumferenceInput bind:value={circumference} />
 		<StateInput bind:value={treeState} />
 		<LocationInput bind:value={location} />
-		<AddressInput bind:value={address} />
 		<NotesInput bind:value={notes} />
 
 		<div class="buttons">
