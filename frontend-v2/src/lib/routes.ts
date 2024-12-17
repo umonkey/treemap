09b38291d4f3c6fd454dc2f4c07c557ba021803c
Justin Forest <hex@umonkey.net>
2024-12-17 22:48:49 +0400

    Add a mobile (Add) button

--- frontend-v2/src/lib/routes.ts
+++ frontend-v2/src/lib/routes.ts
@@ -2,6 +2,7 @@ export const routes = {
 	file: (id: string) => `https://yerevan.treemaps.app/v1/files/${id}.jpg`,
 	search: '/search',
 	searchQuery: (query: string) => `/map?q=${query}`,
+	treeAdd: () => '/add',
 	treeComments: (id: string) => `/tree/${id}/comments`,
 	treeDetails: (id: string) => `/tree/${id}`,
 	treeEdit: (id: string) => `/tree/${id}/edit`,
