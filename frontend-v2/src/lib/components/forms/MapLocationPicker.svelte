6146215c4713a597ffdafdd5403c562bf00e7e41
Justin Forest <hex@umonkey.net>
2024-12-17 18:39:41 +0400

    Fix map sizing

--- frontend-v2/src/lib/components/forms/MapLocationPicker.svelte
+++ frontend-v2/src/lib/components/forms/MapLocationPicker.svelte
@@ -1,7 +1,15 @@
-<div></div>
+<script lang="ts">
+	import Map from '$lib/components/Map.svelte';
+
+	const { center } = $props();
+</script>
+
+<div class="map">
+	<Map {center} />
+</div>
 
 <style>
-	div {
+	.map {
 		background-color: var(--form-background);
 		width: 100%;
 		aspect-ratio: 4/3;
