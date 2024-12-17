44374caac8afcbf337a12413f98f2c9456880f4d
Justin Forest <hex@umonkey.net>
2024-12-17 21:16:44 +0400

    Hide right sidebar on desktop

--- frontend-v2/src/routes/updates/+page.svelte
+++ frontend-v2/src/routes/updates/+page.svelte
@@ -8,5 +8,6 @@
 <div class="padded">
 	<AuthWrapper>
 		<p>This page is under construction. Please come back later.</p>
+		<p>This page will contain all recently added trees, comments and other updates.</p>
 	</AuthWrapper>
 </div>
