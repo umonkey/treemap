cff732f390d886c86be54242515f5a9638274e73
Justin Forest <hex@umonkey.net>
2024-12-17 22:48:49 +0400

    Add a mobile (Add) button

---
+++ frontend-v2/src/lib/components/MobileAddButton.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { routes } from '$lib/routes';
+
+	import PlusIcon from '$lib/icons/PlusIcon.svelte';
+
+	const isHidden = () => {
+		const path = window.location.pathname;
+
+		if (path === '/add') {
+			return true;
+		}
+
+		return false;
+	};
+
+	const handleClick = () => {
+		goto(routes.treeAdd());
+	};
+</script>
+
+<button onclick={handleClick} class:hidden={isHidden()}>
+	<PlusIcon />
+</button>
+
+<style>
+button {
+	display: none;
+}
+
+button.hidden {
+	display: none;
+}
+
+@media (max-width: 480px) {
+	button {
+		display: block;
+		position: fixed;
+		bottom: 60px;
+		right: 10px;
+
+		border: none;
+		border-radius: 50%;
+		outline: none;
+		padding: 2px;
+		cursor: pointer;
+
+		width: 40px;
+		height: 40px;
+
+		color: var(--form-border);
+		background-color: #fff;
+		z-index: var(--z-mobile-nav);
+	}
+}
+</style>
