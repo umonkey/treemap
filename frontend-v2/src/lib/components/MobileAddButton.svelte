cbcc3cd579190a24af6b4e0b30d2e4380211b212
Justin Forest <hex@umonkey.net>
2024-12-18 10:20:33 +0400

    Fix add button issues

--- frontend-v2/src/lib/components/MobileAddButton.svelte
+++ frontend-v2/src/lib/components/MobileAddButton.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
 	import { routes } from '$lib/routes';
 
 	import PlusIcon from '$lib/icons/PlusIcon.svelte';
 
 	const isHidden = () => {
-		const path = window.location.pathname;
+		const path = $page.url.pathname;
 
 		if (path === '/add') {
 			return true;
@@ -24,33 +25,33 @@
 </button>
 
 <style>
-button {
-	display: none;
-}
-
-button.hidden {
-	display: none;
-}
-
-@media (max-width: 480px) {
 	button {
-		display: block;
-		position: fixed;
-		bottom: 60px;
-		right: 10px;
-
-		border: none;
-		border-radius: 50%;
-		outline: none;
-		padding: 2px;
-		cursor: pointer;
-
-		width: 40px;
-		height: 40px;
-
-		color: var(--form-border);
-		background-color: #fff;
-		z-index: var(--z-mobile-nav);
+		display: none;
 	}
-}
+
+	button.hidden {
+		display: none;
+	}
+
+	@media (max-width: 480px) {
+		button {
+			display: block;
+			position: fixed;
+			bottom: 140px;
+			right: 10px;
+
+			border: none;
+			border-radius: 50%;
+			outline: none;
+			padding: 2px;
+			cursor: pointer;
+
+			width: 40px;
+			height: 40px;
+
+			color: var(--form-border);
+			background-color: #fff;
+			z-index: var(--z-add-button);
+		}
+	}
 </style>
