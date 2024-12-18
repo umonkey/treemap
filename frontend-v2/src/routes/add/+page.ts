0ee9a78a9c8114e453ba355e64924fdc4dd5b991
Justin Forest <hex@umonkey.net>
2024-12-18 11:43:54 +0400

    Fix NaN on add tree page

--- frontend-v2/src/routes/add/+page.ts
+++ frontend-v2/src/routes/add/+page.ts
@@ -1,4 +1,5 @@
 import type { Load } from '@sveltejs/kit';
+import { DEFAULT_MAP_CENTER } from '$lib/constants';
 
 export const load: Load = ({
 	url
@@ -6,8 +7,8 @@ export const load: Load = ({
 	lat: number;
 	lng: number;
 }> => {
-	const lat = parseFloat(url.searchParams.get('lat'));
-	const lng = parseFloat(url.searchParams.get('lng'));
+	const lat = parseFloat(url.searchParams.get('lat') ?? DEFAULT_MAP_CENTER[0]);
+	const lng = parseFloat(url.searchParams.get('lng') ?? DEFAULT_MAP_CENTER[1]);
 
 	return {
 		lat,
