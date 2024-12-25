37fcc9d4536e2b8c76c427b9792cdbd445d5eec8
Justin Forest <hex@umonkey.net>
2024-12-25 20:46:06 +0400

    Fix permissions for the target files

--- container/build-yerevan.sh
+++ container/build-yerevan.sh
@@ -37,7 +37,7 @@ docker run --rm \
     -v $PWD/.cache/rust-registry:/root/.cargo/registry \
     -w /app \
     treemap_rust_builder:latest \
-    sh -c "cargo clippy && cargo test && cargo build --release && chown -R 1000:1000 /usr/local/cargo/registry"
+    sh -c "cargo clippy && cargo test && cargo build --release && chown -R 1000:1000 /usr/local/cargo/registry /app/target"
 
 # (4) Test and build the frontend.
 mkdir -p .cache/npm
