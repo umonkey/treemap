3015cf5a84a685171eaa59b0465282cd2dc1e0b5
Justin Forest <hex@umonkey.net>
2024-12-26 14:19:47 +0400

    Fix home page text

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -21,25 +21,19 @@
 <div class="home">
 	<h1>Tree Map of Yerevan</h1>
 	<p>
-		Welcome to the map of trees in Yerevan.<br />We have <strong>{totalCount} trees</strong> in our
-		database.<br />
-		You can read about this app
+		Welcome to the community-drive map of trees in Yerevan, Armenia. Here we collect information
+		about trees to protect them. We have <strong>{totalCount} trees</strong> in our database. You
+		can read about this app
 		<a href="https://github.com/KanachYerevan/kb/wiki/Mobile-Application" target="_blank"
 			>in our wiki</a
 		>.
 	</p>
 
 	<p>
-		Don't know what to do? Find trees around you that are <a href={routes.searchQuery('incomplete')}
+		New here? Find trees around you that are <a href={routes.searchQuery('incomplete')}
 			>missing some data</a
 		>
 		or <a href={routes.searchQuery('noimage')}>missing photos</a> and help us out!
-	</p>
-
-	<p>Please use the navigation bar to access areas of the application.</p>
-	<p>
-		This is a community driven application that everyone can use and update. You can find the source
-		code <a href="https://github.com/umonkey/treemap/" target="_blank">here</a>.
 	</p>
 </div>
 
