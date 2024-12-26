ac9592390a31fb1fa24812909beaf8fe107d3f0d
Justin Forest <hex@umonkey.net>
2024-12-26 14:13:46 +0400

    Fix search presets

--- frontend/src/routes/search/+page.svelte
+++ frontend/src/routes/search/+page.svelte
@@ -34,11 +34,11 @@
 			trees, or <a href={routes.searchQuery('stomp')}>stomps</a>
 		</li>
 		<li>
-			<a href={routes.searchQuery('oak')}>Oaks</a> or
-			<a href={routes.searchQuery('maple')}>maples</a>
+			<a href={routes.searchQuery('quercus')}>Oaks</a> or
+			<a href={routes.searchQuery('acer')}>maples</a>
 			or
-			<a href={routes.searchQuery('ash')}>ashes</a>
-			or <a href={routes.searchQuery('elm')}>elms</a>
+			<a href={routes.searchQuery('fraxinus')}>ashes</a>
+			or <a href={routes.searchQuery('ulmus')}>elms</a>
 		</li>
 		<li>With <a href={routes.searchQuery('unknown hasimage')}>unknown species</a> and photos</li>
 		<li>
