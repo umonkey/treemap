7c6257989d6a584b125a505f8ece46a7ac354206
Justin Forest <hex@umonkey.net>
2025-01-06 22:31:49 +0400

    Translate home page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -21,21 +21,39 @@
 
 <div class="home">
 	<h1>{locale.appTitle()}</h1>
-	<p>
-		Welcome to the community-driven map of trees in Yerevan, Armenia. Here we collect information
-		about trees to protect them. We have <strong>{totalCount} trees</strong> in our database. You
-		can read about this app
-		<a href="https://github.com/KanachYerevan/kb/wiki/Mobile-Application" target="_blank"
-			>in our wiki</a
-		>.
-	</p>
 
-	<p>
-		New here? Find trees around you that are <a href={routes.searchQuery('incomplete')}
-			>missing some data</a
-		>
-		or <a href={routes.searchQuery('noimage')}>missing photos</a> and help us out!
-	</p>
+	{#if navigator.language === 'ru-RU'}
+		<p>
+			Добро пожаловать в карту деревьев Еревана. Здесь мы собираем информацию обо всех деревьях,
+			чтобы защитить их от вырубки. Сейчас у нас в базе данных <strong>{totalCount} деревьев</strong
+			>, добавленных волонтёрами. Подробнее о приложении можно прочитать
+			<a href="https://github.com/KanachYerevan/kb/wiki/Mobile-Application" target="_blank">здесь</a
+			>.
+		</p>
+
+		<p>
+			Впервые здесь? Можно найти рядом с вами деревья, которые <a
+				href={routes.searchQuery('incomplete')}>нуждаются в замерах</a
+			>
+			или <a href={routes.searchQuery('noimage')}>фотографиях</a> и помочь нам!
+		</p>
+	{:else}
+		<p>
+			Welcome to the community-driven map of trees in Yerevan, Armenia. Here we collect information
+			about trees to protect them. We have <strong>{totalCount} trees</strong> in our database. You
+			can read about this app
+			<a href="https://github.com/KanachYerevan/kb/wiki/Mobile-Application" target="_blank"
+				>in our wiki</a
+			>.
+		</p>
+
+		<p>
+			New here? Find trees around you that are <a href={routes.searchQuery('incomplete')}
+				>missing some data</a
+			>
+			or <a href={routes.searchQuery('noimage')}>missing photos</a> and help us out!
+		</p>
+	{/if}
 </div>
 
 <style>
