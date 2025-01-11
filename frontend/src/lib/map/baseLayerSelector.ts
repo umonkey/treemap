a59b6dc7c4be01967deec672bb6ff93da636703a
Justin Forest <hex@umonkey.net>
2025-01-11 17:59:31 +0400

    Update base layers
    - Use higher resolution retina images.
    - Use layers with no trees rendered, to avoid overlap.

--- frontend/src/lib/map/baseLayerSelector.ts
+++ frontend/src/lib/map/baseLayerSelector.ts
@@ -9,23 +9,26 @@ const getDefaultLayer = (): string => {
 
 export const addLayerSelection = (map: L.Map) => {
 	const osmDark = L.tileLayer(
-		'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
+		'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}@2x.png',
 		{
 			attribution:
 				'&copy; <a href="https://myga.am/">Kanach Yerevan</a> &amp; <a href="https://www.openstreetmap.org/copyright">OSM</a>',
 			minZoom: 9,
 			maxZoom: 25,
-			maxNativeZoom: 19
+			maxNativeZoom: 20
 		}
 	);
 
-	const osmLight = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		attribution:
-			'&copy; <a href="https://myga.am/">Kanach Yerevan</a> &amp; <a href="https://www.openstreetmap.org/copyright">OSM</a>',
-		minZoom: 9,
-		maxZoom: 25,
-		maxNativeZoom: 19
-	});
+	const osmLight = L.tileLayer(
+		'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}@2x.png',
+		{
+			attribution:
+				'&copy; <a href="https://myga.am/">Kanach Yerevan</a> &amp; <a href="https://www.openstreetmap.org/copyright">OSM</a>',
+			minZoom: 9,
+			maxZoom: 25,
+			maxNativeZoom: 20
+		}
+	);
 
 	const google = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
 		attribution:
