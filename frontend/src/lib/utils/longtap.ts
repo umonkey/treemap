14e90f465a5c74d770e3bd744643f1b3935f42b6
Justin Forest <hex@umonkey.net>
2025-01-25 13:39:05 +0400

    Attempt to fix long tap

--- frontend/src/lib/utils/longtap.ts
+++ frontend/src/lib/utils/longtap.ts
@@ -1,14 +1,21 @@
+const unselect = () => {
+	document.getSelection().empty();
+};
+
 export const longtap = (el: HTMLElement, callback: () => void, time: number = 500): void => {
 	let timer: number | null = null;
 
-	el.addEventListener('touchstart', (e) => {
-		e.preventDefault();
-
-		timer = setTimeout(() => {
-			timer = null;
-			callback();
-		}, time);
-	});
+	el.addEventListener(
+		'touchstart',
+		() => {
+			timer = setTimeout(() => {
+				timer = null;
+				unselect();
+				callback();
+			}, time);
+		},
+		{ passive: false }
+	);
 
 	el.addEventListener('touchend', () => {
 		clearTimeout(timer);
