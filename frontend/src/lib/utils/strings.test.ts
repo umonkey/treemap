4dfb8c5304879d5cce8b6ba3976effe25af886ba
Justin Forest <hex@umonkey.net>
2025-02-23 14:16:19 +0400

    Add some tests

---
+++ frontend/src/lib/utils/strings.test.ts
@@ -0,0 +1,34 @@
+import { describe, it, expect } from 'vitest';
+import { fileAttribution, formatDate } from './strings';
+import { addUsers } from '$lib/stores/userStore';
+
+describe('formatDate', () => {
+	it('should format correctly', () => {
+		expect(formatDate(1740304662)).toBe('23.02.2025');
+	});
+});
+
+describe('fileAttribution', () => {
+	it('should return an empty string on no data', () => {
+		expect(fileAttribution({} as any)).toBe('');
+	});
+
+	it('should return an empty string on no user', () => {
+		expect(fileAttribution({
+			added_by: 12345,
+		} as any)).toBe('');
+	});
+
+	it('should return the correct attribution', () => {
+		addUsers([{
+			id: 12345,
+			name: 'Alice',
+			picture: 'none.jpg',
+		}]);
+
+		expect(fileAttribution({
+			added_at: 1740304662,
+			added_by: 12345,
+		})).toBe('23.02.2025 by Alice');
+	});
+});
--- frontend/src/lib/utils/strings.ts
+++ frontend/src/lib/utils/strings.ts
@@ -5,8 +5,8 @@ import type { ITreeFile } from '$lib/types';
 export const formatDate = (timestamp: number): string => {
 	const date = new Date(timestamp * 1000);
 
-	const day = ('00' + date.getDate()).substr(-2);
-	const month = ('00' + (date.getMonth() + 1)).substr(-2);
+	const day = ('00' + date.getDate()).slice(-2);
+	const month = ('00' + (date.getMonth() + 1)).slice(-2);
 	const year = date.getFullYear();
 
 	return `${day}.${month}.${year}`;
