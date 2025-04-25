7163a5c44e9a0d82a377bb7caba0282084213c03
Justin Forest <hex@umonkey.net>
2025-04-25 09:48:25 +0400

    Sort changes by date, desc

--- backend/src/common/database/repositories/prop_repository.rs
+++ backend/src/common/database/repositories/prop_repository.rs
@@ -39,7 +39,10 @@ impl PropRepository {
     }
 
     pub async fn find_by_tree(&self, tree_id: u64) -> Result<Vec<PropRecord>> {
-        let query = SelectQuery::new(TABLE).with_condition("tree_id", Value::from(tree_id as i64));
+        let query = SelectQuery::new(TABLE)
+            .with_condition("tree_id", Value::from(tree_id as i64))
+            .with_order_desc("added_at");
+
         self.query_multiple(query).await
     }
 
