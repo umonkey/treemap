7ce27836bbadd4e3eaec10329446e2e9ab412d29
Justin Forest <hex@umonkey.net>
2025-05-01 22:55:50 +0400

    Fix linter errors

--- frontend/src/hooks.client.ts
+++ frontend/src/hooks.client.ts
@@ -4,9 +4,7 @@ import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
 	dsn: 'https://ffcb68c9cff81ad4290341451f9b0623@o4507097921880064.ingest.de.sentry.io/4508460703481936',
-	allowUrls: [
-		/https:\/\/yerevan\.treemaps\.app\//
-	],
+	allowUrls: [/https:\/\/yerevan\.treemaps\.app\//],
 	environment: ENVIRONMENT
 });
 
