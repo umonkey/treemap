88d505643ac47febea58970880cdb53b03742d83
Justin Forest <hex@umonkey.net>
2025-05-25 16:53:01 +0400

    Add stories for auth buttons

---
+++ frontend/src/lib/ui/sign-in-button/SignInButton.stories.svelte
@@ -0,0 +1,11 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import { SignInButton } from '$lib/ui';
+
+	const { Story } = defineMeta({
+		title: 'UI/SignInButton',
+		component: SignInButton,
+	});
+</script>
+
+<Story name="Primary" />
---
+++ frontend/src/lib/ui/sign-out-button/SignOutButton.stories.svelte
@@ -0,0 +1,11 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import { SignOutButton } from '$lib/ui';
+
+	const { Story } = defineMeta({
+		title: 'UI/SignOutButton',
+		component: SignOutButton,
+	});
+</script>
+
+<Story name="Primary" />
