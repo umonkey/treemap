389c825af5d8dfd2d12d6a24786e46e69721bb22
Justin Forest <hex@umonkey.net>
2025-05-26 12:30:18 +0400

    Fix displaying the upload form

--- frontend/src/lib/forms/upload-form/UploadForm.svelte
+++ frontend/src/lib/forms/upload-form/UploadForm.svelte
@@ -14,7 +14,7 @@
 	<!-- loading -->
 {:else if $error}
 	<p>{$error}</p>
-{:else if $tree.files.length > 0}
+{:else if $tree?.files?.length > 0}
 	<h2>Manage existing photos</h2>
 
 	<div class="pics">
