78a473110b3fd3289531c2355d2978a8d02dd914
Justin Forest <hex@umonkey.net>
2025-06-06 11:09:11 +0400

    Refactor MapFullscreen

--- frontend/src/lib/ui/map-fullscreen/MapFullscreen.svelte
+++ frontend/src/lib/ui/map-fullscreen/MapFullscreen.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { hooks } from './hooks';
+	import { MapButton } from '$lib/ui';
+	import ICON from '$lib/assets/fullscreen.svg';
 
-	hooks(onMount);
+	const { handleClick } = hooks({ onMount });
 </script>
+
+<MapButton position="bottomright" icon={ICON} onClick={handleClick} />
