077432b37d9cab8b31958e37e7b533f3a11f940d
Justin Forest <hex@umonkey.net>
2025-07-06 21:34:51 +0400

    Fix linter errors

--- backend/src/services/file_storage/file_storage_selector.rs
+++ backend/src/services/file_storage/file_storage_selector.rs
@@ -1,5 +1,6 @@
 use super::file_storage_interface::FileStorageInterface;
 use super::local_file_storage::LocalFileStorage;
+#[allow(unused_imports)]
 use super::s3_file_storage::S3FileStorage;
 use crate::config::{Config, Secrets};
 use crate::services::{Locatable, Locator};
@@ -11,6 +12,14 @@ pub struct FileStorageSelector {
 }
 
 impl FileStorageSelector {
+    #[cfg(test)]
+    pub async fn new(config: Arc<Config>, _secrets: Arc<Secrets>) -> Result<Self> {
+        Ok(Self {
+            storage: Arc::new(LocalFileStorage::new(config)),
+        })
+    }
+
+    #[cfg(not(test))]
     pub async fn new(config: Arc<Config>, secrets: Arc<Secrets>) -> Result<Self> {
         if let Ok(value) = S3FileStorage::new(secrets).await {
             return Ok(Self {
