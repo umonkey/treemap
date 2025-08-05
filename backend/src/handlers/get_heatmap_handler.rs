d69033a2188ba9289db513a9ea596479e742a293
Justin Forest <hex@umonkey.net>
2025-08-05 20:04:38 +0400

    Fill heat map with missing dates

--- backend/src/handlers/get_heatmap_handler.rs
+++ backend/src/handlers/get_heatmap_handler.rs
@@ -3,6 +3,8 @@
 use crate::services::*;
 use crate::types::*;
 use crate::utils::get_timestamp;
+use chrono::DateTime;
+use std::collections::HashMap;
 use std::sync::Arc;
 
 pub struct GetHeatmapHandler {
@@ -13,22 +15,22 @@ impl GetHeatmapHandler {
     pub async fn handle(&self) -> Result<Vec<HeatmapResponse>> {
         let (after, before) = Self::get_dates();
         let res = self.db.get_heatmap(after, before).await?;
-        Ok(Self::format_data(res))
+        Ok(Self::format_data(after, before, res))
     }
 
     pub async fn handle_user(&self, user_id: u64) -> Result<Vec<HeatmapResponse>> {
         let (after, before) = Self::get_dates();
         let res = self.db.get_user_heatmap(after, before, user_id).await?;
-        Ok(Self::format_data(res))
+        Ok(Self::format_data(after, before, res))
     }
 
-    fn format_data(data: Vec<(String, u64)>) -> Vec<HeatmapResponse> {
-        data.into_iter()
-            .map(|(date, value)| HeatmapResponse {
-                date: date.to_string(),
-                value,
-            })
-            .collect()
+    fn format_data(after: u64, before: u64, data: Vec<(String, u64)>) -> Vec<HeatmapResponse> {
+        let items = data
+            .into_iter()
+            .map(|(date, value)| HeatmapResponse { date, value })
+            .collect();
+
+        Self::normalize_heatmap(after, before, items)
     }
 
     fn get_dates() -> (u64, u64) {
@@ -41,6 +43,49 @@ impl GetHeatmapHandler {
     fn get_today() -> u64 {
         let now = get_timestamp();
         now - (now % 86400)
+    }
+
+    /// Make sure the heat map contains all dates, even when there's no activity.
+    /// This makes sure that days after last activity are being displayed.
+    fn normalize_heatmap(
+        after: u64,
+        before: u64,
+        items: Vec<HeatmapResponse>,
+    ) -> Vec<HeatmapResponse> {
+        let mut map = Self::init_heatmap(after, before);
+
+        for item in items {
+            if map.contains_key(&item.date) {
+                map.insert(item.date.clone(), item);
+            }
+        }
+
+        map.into_values().collect()
+    }
+
+    /// Initialize an empty heatmap with all possible dates from the specified time range.
+    fn init_heatmap(after: u64, before: u64) -> HashMap<String, HeatmapResponse> {
+        let mut map: HashMap<String, HeatmapResponse> = HashMap::new();
+
+        let mut timestamp = after;
+
+        while timestamp <= before {
+            if let Some(dt) = DateTime::from_timestamp(timestamp as i64, 0) {
+                let date_str = dt.format("%Y-%m-%d").to_string();
+
+                map.insert(
+                    date_str.clone(),
+                    HeatmapResponse {
+                        date: date_str,
+                        value: 0,
+                    },
+                );
+            }
+
+            timestamp += 86400; // increment by one day
+        }
+
+        map
     }
 }
 
