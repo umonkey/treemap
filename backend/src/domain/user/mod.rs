422c6c42cca80709db7794b1afeb152f8e9e8ae0
Justin Forest <hex@umonkey.net>
2026-01-11 00:54:14 +0400

    chore: fix a typo

--- backend/src/domain/user/mod.rs
+++ backend/src/domain/user/mod.rs
@@ -1,9 +1,9 @@
 mod models;
 mod repository;
-pub mod schemes;
+mod schemas;
 mod service;
 
 pub use models::User;
 pub use repository::UserRepository;
-pub use schemes::*;
+pub use schemas::UserUpdate;
 pub use service::UserService;
--- backend/src/domain/user/service.rs
+++ backend/src/domain/user/service.rs
@@ -1,6 +1,6 @@
 use super::models::User;
 use super::repository::UserRepository;
-use super::schemes::UserUpdate;
+use super::schemas::UserUpdate;
 use crate::common::database::repositories::UploadRepository;
 use crate::infra::database::{Database, Value};
 use crate::infra::storage::FileStorage;
