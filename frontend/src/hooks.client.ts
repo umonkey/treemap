6852ec640a69dd909e49fd4fb94d3836437d595b
Justin Forest <hex@umonkey.net>
2026-02-24 13:20:48 +0400

    Disable Sentry in dev env

--- frontend/src/hooks.client.ts
+++ frontend/src/hooks.client.ts
@@ -2,20 +2,22 @@ import { ENVIRONMENT } from '$lib/env';
 import { handleErrorWithSentry } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
 
-Sentry.init({
-	dsn: 'https://ffcb68c9cff81ad4290341451f9b0623@o4507097921880064.ingest.de.sentry.io/4508460703481936',
-	allowUrls: [/https:\/\/yerevan\.treemaps\.app\//],
-	environment: ENVIRONMENT,
+if (ENVIRONMENT === 'production') {
+	Sentry.init({
+		dsn: 'https://ffcb68c9cff81ad4290341451f9b0623@o4507097921880064.ingest.de.sentry.io/4508460703481936',
+		allowUrls: [/https:\/\/yerevan\.treemaps\.app\//],
+		environment: ENVIRONMENT,
 
-	// Enable API performance tracing.
-	tracesSampleRate: 1.0,
-	integrations: [
-		Sentry.browserTracingIntegration(),
-		Sentry.captureConsoleIntegration({ levels: ['log', 'warn', 'error'] })
-	],
-	tracePropagationTargets: ['localhost', /^https:\/\/api\.treemaps\.app/],
-	enableLogs: true
-});
+		// Enable API performance tracing.
+		tracesSampleRate: 1.0,
+		integrations: [
+			Sentry.browserTracingIntegration(),
+			Sentry.captureConsoleIntegration({ levels: ['log', 'warn', 'error'] })
+		],
+		tracePropagationTargets: ['localhost', /^https:\/\/api\.treemaps\.app/],
+		enableLogs: true
+	});
+}
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
 export const handleError = handleErrorWithSentry();
