8546620818b548e42a4ad10cd33e06b67033af07
Justin Forest <hex@umonkey.net>
2026-04-04 22:56:56 +0400

    Update search templates

--- frontend/src/routes/search/+page.svelte
+++ frontend/src/routes/search/+page.svelte
@@ -31,76 +31,72 @@
 <Dialog title={locale.searchTitle()} {buttons}>
 	<SearchBar value={query} onInput={handleInput} onSearch={handleSearch} />
 
-	{#if query}
-		<p>
-			<a href={routes.searchQuery(query)} data-testid="search-link">{locale.searchLink(query)}</a>
-		</p>
-		<hr />
-	{/if}
+	<div>
+		{#if query}
+			<p>
+				<a href={routes.searchQuery(query)} data-testid="search-link">{locale.searchLink(query)}</a>
+			</p>
+			<hr />
+		{/if}
 
-	{#if locale.lang === 'ru'}
-		<p>Некоторые запросы для примера:</p>
+		{#if locale.lang === 'ru'}
+			<p>Некоторые запросы для примера:</p>
 
-		<ul>
-			<li>
-				<a href={routes.searchQuery('dead')}>Мёртвые</a>
-				или
-				<a href={routes.searchQuery('gone')}>удалённые</a>
-				деревья, или <a href={routes.searchQuery('stump')}>пни</a>
-			</li>
-			<li>
-				<a href={routes.searchSpecies('quercus')}>Дубы</a> или
-				<a href={routes.searchSpecies('acer')}>клёны</a>
-				или
-				<a href={routes.searchSpecies('fraxinus')}>ясени</a>
-				или <a href={routes.searchSpecies('ulmus')}>вязы</a>
-			</li>
-			<li>
-				<a href={routes.searchQuery('unknown hasimage')}>Неопознанные деревья</a> с фотографиями
-			</li>
-			<li>
-				<a href={routes.searchQuery('noimage')}>Без фотографий</a> или
-				<a href={routes.searchQuery('unknown')}>неопознанные</a>
-			</li>
-			<li>
-				Без <a href={routes.searchQuery('no:height')}>высоты</a>,
-				<a href={routes.searchQuery('no:diameter')}>диаметра</a>,
-				<a href={routes.searchQuery('no:circumference')}>обхвата</a>
-			</li>
-			<li>
-				<a href={routes.searchQuery('incomplete')}>incomplete</a> — деревья без некоторых параметров
-			</li>
-		</ul>
-	{:else}
-		<p>Some interesting searches:</p>
+			<ul>
+				<li>
+					Есть лазер? <a href={routes.searchQuery('no:diameter')}>Без диаметра</a> или
+					<a href={routes.searchQuery('no:height')}>высоты</a>.
+				</li>
+				<li>
+					Есть сантиметр? <a href={routes.searchQuery('no:circumference')}>Без обхвата ствола</a>.
+				</li>
+				<li>Есть камера? <a href={routes.searchQuery('noimage')}>Без фотографий</a>.</li>
+				<li>
+					Есть саженец? <a href={routes.searchQuery('gone')}>Пустые места</a> или
+					<a href={routes.searchQuery('stump')}>пни</a>.
+				</li>
+				<li>
+					<a href={routes.searchQuery('unknown hasimage')}>Неопознанные деревья</a> с фотографиями
+				</li>
+				<li>
+					Без <a href={routes.searchQuery('no:height')}>высоты</a>,
+					<a href={routes.searchQuery('no:diameter')}>диаметра</a>,
+					<a href={routes.searchQuery('no:circumference')}>обхвата</a>
+				</li>
+				<li>
+					<a href={routes.searchQuery('incomplete')}>incomplete</a> — деревья без некоторых параметров
+				</li>
+			</ul>
+		{:else}
+			<p>Some interesting searches:</p>
 
-		<ul>
-			<li>
-				<a href={routes.searchQuery('dead')}>Dead</a>
-				or
-				<a href={routes.searchQuery('gone')}>gone</a>
-				trees, or <a href={routes.searchQuery('stump')}>stumps</a>
-			</li>
-			<li>
-				<a href={routes.searchSpecies('quercus')}>Oaks</a> or
-				<a href={routes.searchSpecies('acer')}>maples</a>
-				or
-				<a href={routes.searchSpecies('fraxinus')}>ashes</a>
-				or <a href={routes.searchSpecies('ulmus')}>elms</a>
-			</li>
-			<li>With <a href={routes.searchQuery('unknown hasimage')}>unknown species</a> and photos</li>
-			<li>
-				With <a href={routes.searchQuery('noimage')}>no images</a> or
-				<a href={routes.searchQuery('unknown')}>no species</a>
-			</li>
-			<li>
-				Without <a href={routes.searchQuery('no:height')}>height</a>,
-				<a href={routes.searchQuery('no:diameter')}>crown diameter</a>,
-				<a href={routes.searchQuery('no:circumference')}>trunk circumference</a>
-			</li>
-			<li><a href={routes.searchQuery('incomplete')}>Missing any data</a></li>
-		</ul>
-	{/if}
+			<ul>
+				<li>
+					<a href={routes.searchQuery('dead')}>Dead</a>,
+					<a href={routes.searchQuery('gone')}>gone</a>
+					trees, or <a href={routes.searchQuery('stump')}>stumps</a>.
+				</li>
+				<li>
+					Have a laser? <a href={routes.searchQuery('no:diameter')}>No crown diameter</a> or
+					<a href={routes.searchQuery('no:height')}>no height</a>.
+				</li>
+				<li>
+					Have a tape measure? <a href={routes.searchQuery('no:circumference')}
+						>No trunk circumference</a
+					>.
+				</li>
+				<li>Have a camera? <a href={routes.searchQuery('noimage')}>No photos</a>.</li>
+				<li>
+					Have a tree? <a href={routes.searchQuery('gone')}>Empty spots</a> or
+					<a href={routes.searchQuery('stump')}>stumps</a>.
+				</li>
+				<li>
+					With <a href={routes.searchQuery('unknown hasimage')}>unknown species</a> but has photos.
+				</li>
+				<li><a href={routes.searchQuery('incomplete')}>Missing some measurements</a>.</li>
+			</ul>
+		{/if}
+	</div>
 
 	<Form onSubmit={handleSubmit}>
 		<SpeciesInput onChange={handleSpeciesChange} />
