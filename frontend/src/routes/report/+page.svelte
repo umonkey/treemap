836e02d3edb86c0c89381c043a6cd04f2d041490
Justin Forest <hex@umonkey.net>
2026-04-05 01:01:36 +0400

    Refactor report page

--- frontend/src/routes/report/+page.svelte
+++ frontend/src/routes/report/+page.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
-	import { locale } from '$lib/locale';
-	import ReportForm from './ReportForm.svelte';
+	import { page } from '$app/stores';
 	import Dialog from '$lib/components/layout/Dialog.svelte';
+	import { locale } from '$lib/locale';
+	import Form from '$lib/ui/form/Form.svelte';
+	import StreetInput from '$lib/ui/street-input/StreetInput.svelte';
+	import StreetReport from '$lib/ui/street-report/StreetReport.svelte';
+	import { hooks } from './hooks';
 
-	const { data } = $props();
+	const address = $derived($page.url.searchParams.get('address'));
+	const { report, handleStreetChange, reload } = hooks();
+
+	$effect(() => {
+		reload(address ?? null);
+	});
 </script>
 
 <Dialog title={locale.reportTitle()}>
-	<ReportForm address={data.address} />
+	<div class="report">
+		<Form>
+			<StreetInput value={address} onChange={handleStreetChange} />
+		</Form>
+	</div>
+
+	{#if $report}
+		<StreetReport data={$report} />
+	{/if}
 </Dialog>
