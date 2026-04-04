6aba5c8930ce09560e4f4e59c82f483096ad7263
Justin Forest <hex@umonkey.net>
2026-04-05 00:03:40 +0400

    Refactor store imports

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import { searchStore } from '$lib/stores';
-	import { rewriteHash } from '$lib/utils/rewrite';
+import { page } from "$app/stores";
+import { searchStore } from "$lib/stores/searchStore";
+import { rewriteHash } from "$lib/utils/rewrite";
 
-	// Redirect old urls from v1.
-	rewriteHash($page.url.hash);
+// Redirect old urls from v1.
+rewriteHash($page.url.hash);
 
-	$effect(() => {
-		const q = $page.url.searchParams.get('q');
-		if (q) {
-			console.debug(`[Home] Extracted q from URL: ${q}`);
-			searchStore.set(q);
-		}
-	});
+$effect(() => {
+	const q = $page.url.searchParams.get("q");
+	if (q) {
+		console.debug(`[Home] Extracted q from URL: ${q}`);
+		searchStore.set(q);
+	}
+});
 </script>
 
 <svelte:head>
