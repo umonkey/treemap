fd09738f8ccc7045efef3115b14d50a95a1dd2d1
Justin Forest <hex@umonkey.net>
2026-04-05 01:18:59 +0400

    Refactor RowSizeInput

--- frontend/src/routes/add/row/RowSizeInput.svelte
+++ frontend/src/routes/add/row/RowSizeInput.svelte
@@ -1,23 +1,27 @@
 <script lang="ts">
 	import { locale } from '$lib/locale';
 	import NumberInput from '$lib/ui/number-input/NumberInput.svelte';
-	import { hooks } from './RowSizeInput.hooks';
+	import { componentState as state } from './RowSizeInput.svelte.ts';
 
 	const { value, distance, autofocus, onChange } = $props<{
-		value: number | null;
+		value: number;
 		distance: number;
 		autofocus?: boolean;
 		onChange: (value: number) => void;
 	}>();
 
-	const { hint, handleChange } = hooks({ distance, value, onChange });
+	$effect(() => {
+		state.distance = distance;
+		state.value = value;
+		state.onChange = onChange;
+	});
 </script>
 
 <NumberInput
 	label={locale.rowSizeLabel()}
-	hint={$hint}
+	hint={state.hint}
 	{value}
-	onChange={handleChange}
+	onChange={state.handleChange}
 	{autofocus}
 	placeholder="2"
 	min="2"
---
+++ frontend/src/routes/add/row/RowSizeInput.svelte.ts
@@ -0,0 +1,21 @@
+import { locale } from '$lib/locale';
+
+export class ComponentState {
+	distance = $state<number>(0);
+	value = $state<number>(0);
+	onChange = $state<(value: number) => void>(() => {});
+
+	hint = $derived.by(() => {
+		const count = this.value;
+		const step = Math.round((this.distance / count) * 10) / 10;
+		return locale.rowStepInfo(count, step);
+	});
+
+	public handleChange = (value: number) => {
+		this.value = value;
+		console.debug(`[map] Row count changed to ${value}`);
+		this.onChange(value);
+	};
+}
+
+export const componentState = new ComponentState();
