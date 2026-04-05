93e086ecd97b726ac74a4be0d0139a165ba0c1f8
Justin Forest <hex@umonkey.net>
2026-04-05 22:01:21 +0400

    Update the duplicates page

--- frontend/src/routes/duplicates/+page.svelte
+++ frontend/src/routes/duplicates/+page.svelte
@@ -9,38 +9,38 @@
 	});
 </script>
 
-<Dialog title={locale.searchTitle()}>
-	<h1>Duplicate trees</h1>
+<Dialog title="Duplicate Trees">
+	<div>
+		{#if pageState.loading}
+			<p>Checking...</p>
+		{:else if pageState.error}
+			<p>{pageState.error.description}</p>
+		{:else if pageState.data && pageState.data.duplicates.length > 0}
+			<p>
+				This page lists trees that have been identified as duplicates, sharing the same coordinates.
+			</p>
+			<p>
+				Please enrich the first tree with data from the duplicates, then remove them (mark as gone).
+			</p>
 
-	{#if pageState.loading}
-		<p>Checking...</p>
-	{:else if pageState.error}
-		<p>{pageState.error.description}</p>
-	{:else if pageState.data && pageState.data.duplicates.length > 0}
-		<p>
-			This page lists trees that have been identified as duplicates, sharing the same coordinates.
-		</p>
-		<p>
-			Please enrich the first tree with data from the duplicates, then remove them (mark as gone).
-		</p>
-
-		<dl>
-			{#each pageState.data.duplicates as dup}
-				<dt>{dup.lat}, {dup.lon}</dt>
-				<dd>
-					<ol>
-						{#each dup.tree_ids as id}
-							<li>
-								<a href={routes.treeDetails(id)}>{id}</a>
-							</li>
-						{/each}
-					</ol>
-				</dd>
-			{/each}
-		</dl>
-	{:else}
-		<p>Congratulations, there are no duplicate trees!</p>
-	{/if}
+			<dl>
+				{#each pageState.data.duplicates as dup}
+					<dt>{dup.lat}, {dup.lon}</dt>
+					<dd>
+						<ol>
+							{#each dup.tree_ids as id}
+								<li>
+									<a href={routes.mapPreview(id)}>{id}</a>
+								</li>
+							{/each}
+						</ol>
+					</dd>
+				{/each}
+			</dl>
+		{:else}
+			<p>Congratulations, there are no duplicate trees!</p>
+		{/if}
+	</div>
 </Dialog>
 
 <style>
