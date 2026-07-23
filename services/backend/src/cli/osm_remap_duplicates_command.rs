30bcc23ae9412f636b0518a6093b6544637cb142
Justin Forest <hex@umonkey.net>
2026-07-23 11:08:30 +0400

    feat(backend): add transactions to some CLI commands

--- services/backend/src/cli/osm_remap_duplicates_command.rs
+++ services/backend/src/cli/osm_remap_duplicates_command.rs
@@ -4,7 +4,10 @@ use crate::services::*;
 pub async fn osm_remap_duplicates_command() {
     let state = AppState::new()
         .await
-        .expect("Error initializing app state.");
+        .expect("Error initializing app state.")
+        .session()
+        .await
+        .expect("Error creating session state.");
 
     let merger = state
         .build::<TreeMergerService>()
@@ -14,6 +17,12 @@ pub async fn osm_remap_duplicates_command() {
         .remap_osm_duplicates()
         .await
         .expect("Error remapping OSM duplicates.");
+
+    state
+        .database
+        .commit()
+        .await
+        .expect("Error committing transaction.");
 
     for (main_id, dup_id) in remapped {
         println!(
