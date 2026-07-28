6312276d9f89746aba7443b7c9e0bbb74f8d2618
Justin Forest <hex@umonkey.net>
2026-07-29 01:44:34 +0400

    define panoramas bucket policy

--- infra/digitalocean.tf
+++ infra/digitalocean.tf
@@ -24,3 +24,20 @@ resource "digitalocean_project_resources" "treemap" {
   project   = var.do_project_id
   resources = [digitalocean_spaces_bucket.panoramas.urn]
 }
+
+resource "aws_s3_bucket_policy" "panoramas" {
+  provider = aws.spaces
+  bucket   = digitalocean_spaces_bucket.panoramas.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::panoramas-px3qodu3/*"
+      }
+    ]
+  })
+}
--- infra/provider.tf
+++ infra/provider.tf
@@ -2,6 +2,25 @@ provider "aws" {
   region = var.region
 }
 
+# DigitalOcean Spaces is S3-compatible, but the DigitalOcean Terraform provider 
+# does not currently support bucket policies. We use the AWS provider with 
+# a custom endpoint to manage these policies.
+provider "aws" {
+  alias                       = "spaces"
+  region                      = "us-east-1" # DigitalOcean ignores this when using custom endpoints
+  access_key                  = var.do_spaces_access_id
+  secret_key                  = var.do_spaces_secret_key
+  s3_use_path_style           = false
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true # Necessary because 'fra1' is not a standard AWS region
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3 = "https://fra1.digitaloceanspaces.com"
+  }
+}
+
 provider "digitalocean" {
   token             = var.do_token
   spaces_access_id  = var.do_spaces_access_id
