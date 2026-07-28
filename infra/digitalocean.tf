d835c075be65f6bf711321665410af34cf3ba4f7
Justin Forest <hex@umonkey.net>
2026-07-29 03:54:14 +0400

    update DO buckets

--- infra/digitalocean.tf
+++ infra/digitalocean.tf
@@ -20,9 +20,30 @@ resource "digitalocean_cdn" "panoramas" {
   origin = digitalocean_spaces_bucket.panoramas.bucket_domain_name
 }
 
+resource "digitalocean_spaces_bucket" "panoramas_tmp" {
+  name   = "treemap-panoramas-tmp-g6dxkoua"
+  region = "fra1"
+}
+
+resource "digitalocean_spaces_bucket_cors_configuration" "panoramas_tmp" {
+  region = digitalocean_spaces_bucket.panoramas_tmp.region
+  bucket = digitalocean_spaces_bucket.panoramas_tmp.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "PUT", "POST"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
+
 resource "digitalocean_project_resources" "treemap" {
   project   = var.do_project_id
-  resources = [digitalocean_spaces_bucket.panoramas.urn]
+  resources = [
+    digitalocean_spaces_bucket.panoramas.urn,
+    digitalocean_spaces_bucket.panoramas_tmp.urn,
+  ]
 }
 
 resource "aws_s3_bucket_policy" "panoramas" {
