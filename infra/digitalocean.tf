b41e5dec22da052a51d324c48af8b919121fd2c4
Justin Forest <hex@umonkey.net>
2026-07-29 04:04:25 +0400

    import remaining buckets to terraform

--- infra/digitalocean.tf
+++ infra/digitalocean.tf
@@ -39,11 +39,121 @@ resource "digitalocean_spaces_bucket_cors_configuration" "panoramas_tmp" {
 }
 
 resource "digitalocean_project_resources" "treemap" {
-  project   = var.do_project_id
+  project = var.do_project_id
   resources = [
     digitalocean_spaces_bucket.panoramas.urn,
     digitalocean_spaces_bucket.panoramas_tmp.urn,
+    digitalocean_spaces_bucket.backups.urn,
+    digitalocean_spaces_bucket.dataset.urn,
+    digitalocean_spaces_bucket.tiles.urn,
+    digitalocean_spaces_bucket.treemap.urn,
   ]
+}
+
+resource "digitalocean_spaces_bucket" "backups" {
+  name   = "treemap-backups-uzbmpe2a"
+  region = "fra1"
+}
+
+resource "digitalocean_spaces_bucket" "dataset" {
+  name   = "treemap-dataset"
+  region = "fra1"
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "dataset" {
+  provider = aws.spaces
+  bucket   = digitalocean_spaces_bucket.dataset.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::treemap-dataset/*"
+      }
+    ]
+  })
+}
+
+resource "digitalocean_spaces_bucket" "tiles" {
+  name   = "treemap-tiles"
+  region = "fra1"
+  acl    = "public-read"
+}
+
+resource "digitalocean_spaces_bucket_cors_configuration" "tiles" {
+  region = digitalocean_spaces_bucket.tiles.region
+  bucket = digitalocean_spaces_bucket.tiles.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
+}
+
+resource "digitalocean_cdn" "tiles" {
+  origin = digitalocean_spaces_bucket.tiles.bucket_domain_name
+}
+
+resource "aws_s3_bucket_policy" "tiles" {
+  provider = aws.spaces
+  bucket   = digitalocean_spaces_bucket.tiles.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::treemap-tiles/*"
+      }
+    ]
+  })
+}
+
+resource "digitalocean_spaces_bucket" "treemap" {
+  name   = "treemap"
+  region = "fra1"
+  acl    = "public-read"
+}
+
+resource "digitalocean_spaces_bucket_cors_configuration" "treemap" {
+  region = digitalocean_spaces_bucket.treemap.region
+  bucket = digitalocean_spaces_bucket.treemap.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "PUT", "POST", "HEAD"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
+}
+
+resource "digitalocean_cdn" "treemap" {
+  origin = digitalocean_spaces_bucket.treemap.bucket_domain_name
+}
+
+resource "aws_s3_bucket_policy" "treemap" {
+  provider = aws.spaces
+  bucket   = digitalocean_spaces_bucket.treemap.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::treemap/*"
+      }
+    ]
+  })
 }
 
 resource "aws_s3_bucket_policy" "panoramas" {
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,3 +2,13 @@ output "panoramas_cdn_url" {
   value       = "https://${digitalocean_cdn.panoramas.endpoint}"
   description = "The URL of the CDN endpoint for the panoramas bucket"
 }
+
+output "tiles_cdn_url" {
+  value       = "https://${digitalocean_cdn.tiles.endpoint}"
+  description = "The URL of the CDN endpoint for the tiles bucket"
+}
+
+output "treemap_cdn_url" {
+  value       = "https://${digitalocean_cdn.treemap.endpoint}"
+  description = "The URL of the CDN endpoint for the main treemap bucket"
+}
