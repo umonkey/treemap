5e9beea513d5af01bd77f637b629cde17a97060a
Justin Forest <hex@umonkey.net>
2026-08-20 23:43:08 +0400

    update PanoramicLayer to use mapBus.bounds

--- services/frontend/src/lib/components/map/PanoramicLayer.svelte.ts
+++ services/frontend/src/lib/components/map/PanoramicLayer.svelte.ts
@@ -2,12 +2,13 @@ import { getPanoramasGeoJSON } from '$lib/api/panoramas';
 import { mapBus } from '$lib/buses/mapBus';
 import { showError } from '$lib/errors';
 import { goto, routes } from '$lib/routes';
-import { mapPoiStore } from '$lib/stores/mapPoi.svelte';
 import { mapMarkerStore } from '$lib/stores/mapMarker.svelte';
+import { mapPoiStore } from '$lib/stores/mapPoi.svelte';
+import { mapZoom } from '$lib/stores/mapStore';
+import type { IBounds } from '$lib/types';
 import { Debouncer } from '$lib/utils/debounce';
-import { LngLat, type Map } from 'maplibre-gl';
-import { getMapContext } from 'svelte-maplibre';
-import { MapBouncer } from './MapBouncer';
+import { LngLat } from 'maplibre-gl';
+import { get } from 'svelte/store';
 
 type Properties = {
 	id: string;
@@ -30,7 +31,7 @@ type Collection = {
 	features: Feature[];
 };
 
-const extendBounds = ({ n, e, s, w }: { n: number; e: number; s: number; w: number }) => {
+const extendBounds = ({ n, e, s, w }: IBounds): IBounds => {
 	const dLat = n - s;
 	const dLon = e - w;
 
@@ -42,21 +43,18 @@ const extendBounds = ({ n, e, s, w }: { n: number; e: number; s: number; w: numb
 	};
 };
 
-class PanoramicLayerState {
+export class PanoramicLayerState {
 	data = $state.raw<Collection | undefined>(undefined);
+	bounds = $state<IBounds | undefined>(undefined);
 	fetchDebouncer = new Debouncer(200);
-	moveBouncer = new MapBouncer();
 
-	private reload = (map: Map) => {
-		const bounds = map.getBounds();
-		const zoom = map.getZoom();
+	private reload = () => {
+		if (!this.bounds) {
+			return;
+		}
 
-		const { n, s, e, w } = extendBounds({
-			n: bounds.getNorth(),
-			s: bounds.getSouth(),
-			e: bounds.getEast(),
-			w: bounds.getWest()
-		});
+		const zoom = get(mapZoom);
+		const { n, s, e, w } = extendBounds(this.bounds);
 
 		this.fetchDebouncer.run(() => {
 			getPanoramasGeoJSON(n, e, s, w, zoom >= 18, true)
@@ -79,15 +77,9 @@ class PanoramicLayerState {
 		});
 	};
 
-	private handleMove = (map: Map) => {
-		const bounds = map.getBounds();
-
-		if (!this.moveBouncer.changed(bounds)) {
-			console.debug('PanoramicLayer reload triggered, but map not moved.');
-			return;
-		}
-
-		this.reload(map);
+	private handleBounds = (bounds: IBounds) => {
+		this.bounds = bounds;
+		this.reload();
 	};
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -113,26 +105,13 @@ class PanoramicLayerState {
 	};
 
 	public onMount = () => {
-		const map = getMapContext()?.map;
-
-		if (!map) {
-			console.warn('Map not available, cannot display panoramas.');
-			return;
-		}
-
-		const handleMove = () => this.handleMove(map);
-		const reload = () => this.reload(map);
-
-		mapBus.on('reload', reload);
-
-		map.on('moveend', handleMove);
-		map.on('zoomend', handleMove);
-		reload();
+		mapBus.on('bounds', this.handleBounds);
+		mapBus.on('reload', this.reload);
 
 		return () => {
-			mapBus.off('reload', reload);
-			map.off('moveend', handleMove);
-			map.off('zoomend', handleMove);
+			this.bounds = undefined;
+			mapBus.off('bounds', this.handleBounds);
+			mapBus.off('reload', this.reload);
 		};
 	};
 }
