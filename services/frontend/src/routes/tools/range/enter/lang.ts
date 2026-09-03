1f0a0da685d67f2fb9018b3dfce976f7ffba0c5a
Justin Forest <hex@umonkey.net>
2026-09-03 14:28:03 +0400

    update strings

--- services/frontend/src/routes/tools/range/enter/lang.ts
+++ services/frontend/src/routes/tools/range/enter/lang.ts
@@ -1,7 +1,7 @@
 export const lang = {
 	title: 'Trilateration Tool',
 	intro:
-		'Enter the measured laser distance (in meters) from each Ground Control Point to the target tree.',
+		'Enter the measured laser distance from each Ground Control Point to the target tree.',
 	gcpRadiusLabel: (label: string) => `Distance to GCP #${label}, meters`,
 
 	back: 'Change GCP',
