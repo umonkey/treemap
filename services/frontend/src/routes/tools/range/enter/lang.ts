90d2c448376032058ba130e474f794f990be2170
Justin Forest <hex@umonkey.net>
2026-09-03 14:20:22 +0400

    change labels

--- services/frontend/src/routes/tools/range/enter/lang.ts
+++ services/frontend/src/routes/tools/range/enter/lang.ts
@@ -4,7 +4,7 @@ export const lang = {
 		'Enter the measured laser distance (in meters) from each Ground Control Point to the target tree.',
 	gcpRadiusLabel: (label: string) => `Distance to GCP #${label}, meters`,
 
-	back: 'Back to Setup',
+	back: 'Change GCP',
 	addTree: 'Add tree',
 	recordedTrees: 'Recorded Trees:',
 	removeTree: 'Remove'
